1702ff84e7a081303d6c3bca3585e2572a8e7fd1
Rafał Przędzik <rafal.przedzik@strix.net>
2019-07-05 13:23:11 +0200

    ERG-#1 - fix abstratc product serialization

--- module/product/src/Domain/Entity/AbstractProduct.php
+++ module/product/src/Domain/Entity/AbstractProduct.php
@@ -27,6 +27,7 @@ use Ergonode\Product\Domain\ValueObject\ProductStatus;
 use Ergonode\Product\Domain\ValueObject\Sku;
 use Ergonode\Value\Domain\ValueObject\StringValue;
 use Ergonode\Value\Domain\ValueObject\ValueInterface;
+use JMS\Serializer\Annotation as JMS;
 
 /**
  */
@@ -56,11 +57,15 @@ abstract class AbstractProduct extends AbstractAggregateRoot
 
     /**
      * @var ValueInterface[]
+     *
+     * @JMS\Type("array<string, Ergonode\Value\Domain\ValueObject\ValueInterface>")
      */
     private $attributes;
 
     /**
      * @var string[]
+     *
+     * @JMS\Type("array<string>")
      */
     private $categories;
 
