e2f139e641c31b1f8357c4a7a8693863763b79be
sebastian <bastek.bielawski@gmail.com>
2019-08-13 20:48:47 +0200

    ValueInterface JMS handler

--- module/value/src/Domain/Service/Strategy/CollectionValueUpdateStrategy.php
+++ module/value/src/Domain/Service/Strategy/CollectionValueUpdateStrategy.php
@@ -9,9 +9,9 @@ declare(strict_types = 1);
 
 namespace Ergonode\Value\Domain\Service\Strategy;
 
+use Ergonode\Value\Domain\Service\ValueUpdateStrategyInterface;
 use Ergonode\Value\Domain\ValueObject\CollectionValue;
 use Ergonode\Value\Domain\ValueObject\ValueInterface;
-use Ergonode\Value\Domain\Service\ValueUpdateStrategyInterface;
 
 /**
  */
@@ -28,10 +28,7 @@ class CollectionValueUpdateStrategy implements ValueUpdateStrategyInterface
     }
 
     /**
-     * @param ValueInterface|CollectionValue $oldValue
-     * @param ValueInterface|CollectionValue $newValue
-     *
-     * @return ValueInterface
+     * {@inheritDoc}
      */
     public function calculate(ValueInterface $oldValue, ValueInterface $newValue): ValueInterface
     {
