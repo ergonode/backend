c0a03709af2535602f42eaac195ad033150507cd
Mateusz Kolankowski <wiewiurdp@tlen.pl>
2019-09-16 13:35:47 +0200

    Create boolean data transformer (#139)
    * creating Data Transformer
    
    * adding test to BooleanDataTransformer
    
    * fixing
    
    * adding data provider

---
+++ module/core/src/Application/Form/DataTransformer/BooleanDataTransformer.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Core\Application\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+/**
+ */
+class BooleanDataTransformer implements DataTransformerInterface
+{
+    /**
+     * @param mixed $value
+     *
+     * @return bool|int|mixed
+     */
+    public function transform($value)
+    {
+        if ('true' === $value) {
+            return 1;
+        }
+        if ('false' === $value) {
+            return false;
+        }
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return bool|mixed
+     */
+    public function reverseTransform($value)
+    {
+        if (1 == $value || 'true' === $value) {
+            return 'true';
+        }
+        if (false == $value || 'false' === $value) {
+            return 'false';
+        }
+        throw new TransformationFailedException('Expect boolean');
+    }
+}
