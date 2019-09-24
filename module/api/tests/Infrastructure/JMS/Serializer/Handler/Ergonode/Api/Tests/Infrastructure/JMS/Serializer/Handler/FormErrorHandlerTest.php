928d5f119fa31eff7ca1505ce8ab3cd447bd48b2
mateusz.kolankowski <wiewiurdp@tlen.pl>
2019-09-24 15:06:14 +0200

    Adding Handler tests

---
+++ module/api/tests/Infrastructure/JMS/Serializer/Handler/Ergonode/Api/Tests/Infrastructure/JMS/Serializer/Handler/FormErrorHandlerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Api\Tests\Infrastructure\JMS\Serializer\Handler;
+
+use Ergonode\Api\Infrastructure\JMS\Serializer\Handler\FormErrorHandler;
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+class FormErrorHandlerTest extends TestCase
+{
+
+    /**
+     */
+    public function testConfiguration(): void
+    {
+        $configurations = FormErrorHandler::getSubscribingMethods();
+        foreach ($configurations as $configuration) {
+            $this->assertArrayHasKey('direction', $configuration);
+            $this->assertArrayHasKey('type', $configuration);
+            $this->assertArrayHasKey('format', $configuration);
+            $this->assertArrayHasKey('method', $configuration);
+        }
+    }
+}
---
+++ module/api/tests/Infrastructure/JMS/Serializer/Handler/Ergonode/Api/Tests/Infrastructure/JMS/Serializer/Handler/FormValidationExceptionHandlerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Api\Tests\Infrastructure\JMS\Serializer\Handler;
+
+use Ergonode\Api\Infrastructure\JMS\Serializer\Handler\FormValidationExceptionHandler;
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+class FormValidationExceptionHandlerTest extends TestCase
+{
+    /**
+     */
+    public function testConfiguration(): void
+    {
+        $configurations = FormValidationExceptionHandler::getSubscribingMethods();
+        foreach ($configurations as $configuration) {
+            $this->assertArrayHasKey('direction', $configuration);
+            $this->assertArrayHasKey('type', $configuration);
+            $this->assertArrayHasKey('format', $configuration);
+            $this->assertArrayHasKey('method', $configuration);
+        }
+    }
+}
