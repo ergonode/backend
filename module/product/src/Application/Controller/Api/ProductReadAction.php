9079498a4cf451776755c12d0d60fa8344e10644
Sebastian Bielawski <bastek.bielawski@gmail.com>
2019-10-07 09:24:57 +0200

    Product module (ADR) (#178)
    #173 Product module (ADR)

---
+++ module/product/src/Application/Controller/Api/ProductReadAction.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Product\Application\Controller\Api;
+
+use Ergonode\Api\Application\Response\SuccessResponse;
+use Ergonode\Core\Domain\ValueObject\Language;
+use Ergonode\Product\Domain\Entity\AbstractProduct;
+use Ergonode\Product\Domain\Query\GetProductQueryInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("products/{product}", methods={"GET"}, requirements={"product"="[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"})
+ */
+class ProductReadAction
+{
+    /**
+     * @var GetProductQueryInterface
+     */
+    private $getProductQuery;
+
+    /**
+     * @param GetProductQueryInterface $getProductQuery
+     */
+    public function __construct(GetProductQueryInterface $getProductQuery)
+    {
+        $this->getProductQuery = $getProductQuery;
+    }
+
+    /**
+     * @IsGranted("PRODUCT_READ")
+     *
+     * @SWG\Tag(name="Product")
+     * @SWG\Parameter(
+     *     name="product",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     description="product ID",
+     * )
+     * @SWG\Parameter(
+     *     name="language",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     default="EN",
+     *     description="Language Code",
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns import",
+     * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Not found",
+     * )
+     *
+     * @param AbstractProduct $product
+     * @param Language        $language
+     *
+     * @ParamConverter(class="Ergonode\Product\Domain\Entity\AbstractProduct")
+     *
+     * @return Response
+     */
+    public function __invoke(AbstractProduct $product, Language $language): Response
+    {
+        $result =  $this->getProductQuery->query($product->getId(), $language);
+
+        return new SuccessResponse($result);
+    }
+}
