5bde1a58743d6ce33ab19e8ef169c10632558bb1
Bastek Bielawski <bastek.bielawski@gmail.com>
2019-10-09 11:37:16 +0200

    Category fixes

--- module/category/src/Infrastructure/Grid/CategoryGrid.php
+++ module/category/src/Infrastructure/Grid/CategoryGrid.php
@@ -47,12 +47,17 @@ class CategoryGrid extends AbstractGrid
         $id = new TextColumn('id', 'Id');
         $id->setVisible(false);
         $this->addColumn('id', $id);
+
         $index = new IntegerColumn('sequence', $this->trans('Index'), new TextFilter($filters->getString('sequence')));
         $this->addColumn('sequence', $index);
+
+        $this->addColumn('code', new TextColumn('code', 'Code', new TextFilter($filters->getString('code'))));
+
         $name = new TextColumn('name', 'Name', new TextFilter($filters->getString('name')));
         $this->addColumn('name', $name);
-        $this->addColumn('code', new TextColumn('code', 'Code', new TextFilter($filters->getString('code'))));
+
         $this->addColumn('elements_count', new IntegerColumn('elements_count', $this->trans('Number of products'), new TextFilter($filters->getString('elements_count'))));
+
         $this->addColumn('_links', new LinkColumn('hal', [
             'get' => [
                 'route' => 'ergonode_category_read',
@@ -69,6 +74,7 @@ class CategoryGrid extends AbstractGrid
                 'method' => Request::METHOD_DELETE,
             ],
         ]));
+
         $this->orderBy('sequence', 'DESC');
         $this->setConfiguration(self::PARAMETER_ALLOW_COLUMN_RESIZE, false);
     }
