653815b6a70d8bfd71402c12047fd37de8fd7b42
Bastek Bielawski <bastek.bielawski@gmail.com>
2019-10-09 16:41:14 +0200

    Workflow module (ADR)

---
+++ module/workflow/src/Application/Controller/Api/Transition/TransitionGridReadAction.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Workflow\Application\Controller\Api\Transition;
+
+use Ergonode\Api\Application\Response\SuccessResponse;
+use Ergonode\Core\Domain\ValueObject\Language;
+use Ergonode\Grid\Renderer\GridRenderer;
+use Ergonode\Grid\RequestGridConfiguration;
+use Ergonode\Workflow\Domain\Entity\Workflow;
+use Ergonode\Workflow\Domain\Query\TransitionQueryInterface;
+use Ergonode\Workflow\Infrastructure\Grid\TransitionGrid;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(
+ *     name="ergonode_workflow_transition_read",
+ *     path="/workflow/default/transitions",
+ *     methods={"GET"}
+ * )
+ */
+class TransitionGridReadAction
+{
+    /**
+     * @var TransitionQueryInterface
+     */
+    private $query;
+
+    /**
+     * @var TransitionGrid
+     */
+    private $grid;
+
+    /**
+     * @var GridRenderer
+     */
+    private $gridRenderer;
+
+    /**
+     * @param GridRenderer             $gridRenderer
+     * @param TransitionQueryInterface $query
+     * @param TransitionGrid           $grid
+     */
+    public function __construct(
+        GridRenderer $gridRenderer,
+        TransitionQueryInterface $query,
+        TransitionGrid $grid
+    ) {
+        $this->query = $query;
+        $this->grid = $grid;
+        $this->gridRenderer = $gridRenderer;
+    }
+
+    /**
+     * @IsGranted("WORKFLOW_READ")
+     *
+     * @SWG\Tag(name="Workflow")
+     * @SWG\Parameter(
+     *     name="language",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     default="EN",
+     *     description="Language Code",
+     * )
+     * @SWG\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     type="integer",
+     *     required=true,
+     *     default="50",
+     *     description="Number of returned lines",
+     * )
+     * @SWG\Parameter(
+     *     name="offset",
+     *     in="query",
+     *     type="integer",
+     *     required=true,
+     *     default="0",
+     *     description="Number of start line",
+     * )
+     * @SWG\Parameter(
+     *     name="field",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     description="Order field",
+     * )
+     * @SWG\Parameter(
+     *     name="order",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     enum={"ASC","DESC"},
+     *     description="Order",
+     * )
+     * @SWG\Parameter(
+     *     name="filter",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     description="Filter"
+     * )
+     * @SWG\Parameter(
+     *     name="show",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     enum={"COLUMN","DATA"},
+     *     description="Specify what response should containts"
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns statuses collection",
+     * )
+     *
+     * @ParamConverter(class="Ergonode\Workflow\Domain\Entity\Workflow")
+     * @ParamConverter(class="Ergonode\Grid\RequestGridConfiguration")
+     *
+     * @param Workflow                 $workflow
+     * @param Language                 $language
+     * @param RequestGridConfiguration $configuration
+     *
+     * @return Response
+     */
+    public function __invoke(Workflow $workflow, Language $language, RequestGridConfiguration $configuration): Response
+    {
+        $data = $this->gridRenderer->render(
+            $this->grid,
+            $configuration,
+            $this->query->getDataSet($workflow->getId(), $language),
+            $language
+        );
+
+        return new SuccessResponse($data);
+    }
+}
---
+++ module/workflow/src/Application/Controller/Api/Workflow/WorkflowReadAction.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Workflow\Application\Controller\Api\Workflow;
+
+use Ergonode\Api\Application\Response\SuccessResponse;
+use Ergonode\Workflow\Domain\Entity\Workflow;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(
+ *     name="ergonode_workflow_read",
+ *     path="/workflow/default",
+ *     methods={"GET"}
+ * )
+ */
+class WorkflowReadAction
+{
+    /**
+     * @IsGranted("WORKFLOW_READ")
+     *
+     * @SWG\Tag(name="Workflow")
+     * @SWG\Parameter(
+     *     name="language",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     default="EN",
+     *     description="Language Code",
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns attribute",
+     * )
+     * @SWG\Response(
+     *     response=404,
+     *     description="Not found",
+     * )
+     *
+     * @ParamConverter(class="Ergonode\Workflow\Domain\Entity\Workflow")
+     *
+     * @param Workflow $workflow
+     *
+     * @return Response
+     */
+    public function __invoke(Workflow $workflow): Response
+    {
+        return new SuccessResponse($workflow);
+    }
+}
