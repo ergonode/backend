5456acfacb8a25909844503ff486f5f0b632c61d
Rafał Przędzik <rafal.przedzik@strix.net>
2019-11-13 13:21:22 +0100

    add some tests

---
+++ module/category/tests/Application/Form/CategoryUpdateFormTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Category\Tests\Application\Form;
+
+use Ergonode\Category\Application\Form\CategoryUpdateForm;
+use Ergonode\Category\Application\Model\CategoryUpdateFormModel;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+/**
+ */
+class CategoryUpdateFormTest extends TypeTestCase
+{
+    /**
+     */
+    public function testSubmitValidData(): void
+    {
+        $formData = [
+            'name' => ['PL' =>  'Any Name'],
+        ];
+
+        $object = new CategoryUpdateFormModel();
+        $object->name = ['PL' =>  'Any Name'];
+
+        $objectToCompare = new CategoryUpdateFormModel();
+        $form = $this->factory->create(CategoryUpdateForm::class, $objectToCompare);
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertTrue($form->isValid());
+        $this->assertEquals($object, $objectToCompare);
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
