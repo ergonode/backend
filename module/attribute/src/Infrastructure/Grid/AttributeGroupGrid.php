0268539a16e277886c6559d2cd4351bb699d651f
Rafał Przędzik <rafal.przedzik@strix.net>
2019-11-25 08:59:16 +0100

    add missing attribute group grid links

--- module/attribute/src/Infrastructure/Grid/AttributeGroupGrid.php
+++ module/attribute/src/Infrastructure/Grid/AttributeGroupGrid.php
@@ -12,9 +12,11 @@ namespace Ergonode\Attribute\Infrastructure\Grid;
 use Ergonode\Core\Domain\ValueObject\Language;
 use Ergonode\Grid\AbstractGrid;
 use Ergonode\Grid\Column\IntegerColumn;
+use Ergonode\Grid\Column\LinkColumn;
 use Ergonode\Grid\Column\TextColumn;
 use Ergonode\Grid\Filter\TextFilter;
 use Ergonode\Grid\GridConfigurationInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
@@ -46,6 +48,27 @@ class AttributeGroupGrid extends AbstractGrid
         $this->addColumn('code', new TextColumn('code', $this->trans('Code'), new TextFilter($filters->get('code'))));
         $this->addColumn('name', new TextColumn('name', $this->trans('Name'), new TextFilter($filters->get('name'))));
         $this->addColumn('elements_count', new IntegerColumn('elements_count', $this->trans('Elements Count'), new TextFilter($filters->get('elements_count'))));
+
+        $links = [
+            'get' => [
+                'privilege' => 'ATTRIBUTE_GROUP_READ',
+                'route' => 'ergonode_attribute_group_read',
+                'parameters' => ['language' => $language->getCode(), 'group' => '{id}'],
+            ],
+            'edit' => [
+                'privilege' => 'ATTRIBUTE_GROUP_UPDATE',
+                'route' => 'ergonode_attribute_group_change',
+                'parameters' => ['language' => $language->getCode(), 'group' => '{id}'],
+                'method' => Request::METHOD_PUT,
+            ],
+            'delete' => [
+                'privilege' => 'ATTRIBUTE_GROUP_DELETE',
+                'route' => 'ergonode_attribute_group_delete',
+                'parameters' => ['language' => $language->getCode(), 'group' => '{id}'],
+                'method' => Request::METHOD_DELETE,
+            ],
+        ];
+        $this->addColumn('_links', new LinkColumn('hal', $links));
     }
 
     /**
