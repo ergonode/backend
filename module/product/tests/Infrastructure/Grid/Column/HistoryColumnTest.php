39b75cb560323cd2f755243f98e70499287d4e38
mateusz.kolankowski <wiewiurdp@tlen.pl>
2019-11-26 15:14:02 +0100

    adding product history endpoint

---
+++ module/product/tests/Infrastructure/Grid/Column/HistoryColumnTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Tests\Product\Infrastructure\Grid\Column;
+
+use Ergonode\Core\Domain\ValueObject\Language;
+use Ergonode\Product\Infrastructure\Grid\Column\HistoryColumn;
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+class HistoryColumnTest extends TestCase
+{
+
+    /**
+     */
+    public function testGetters(): void
+    {
+        $field = 'Any field';
+        $parameter = 'Any parameter';
+        $label = 'Any label';
+        $language = $this->createMock(Language::class);
+        $column = new HistoryColumn($field, $parameter, $label, $language);
+        $this->assertSame($field, $column->getField());
+        $this->assertSame($parameter, $column->getParameterField());
+        $this->assertSame($label, $column->getLabel());
+        $this->assertSame($language, $column->getLanguage());
+        $this->assertSame(HistoryColumn::TYPE, $column->getType());
+    }
+}
