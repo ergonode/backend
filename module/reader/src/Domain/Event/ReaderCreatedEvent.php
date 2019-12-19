2f842eeea12382fdddd299857792ffb2a12c8d41
Rafał Przędzik <rafal.przedzik@strix.net>
2019-12-19 08:46:08 +0100

    Refactoring reader domain events

--- module/reader/src/Domain/Event/ReaderCreatedEvent.php
+++ module/reader/src/Domain/Event/ReaderCreatedEvent.php
@@ -9,14 +9,15 @@ declare(strict_types = 1);
 
 namespace Ergonode\Reader\Domain\Event;
 
-use Ergonode\EventSourcing\Infrastructure\DomainEventInterface;
+use Ergonode\Core\Domain\Entity\AbstractId;
+use Ergonode\EventSourcing\Infrastructure\DomainAggregateEventInterface;
 use Ergonode\Reader\Domain\Entity\ReaderId;
 use Ergonode\Reader\Domain\FormatterInterface;
 use JMS\Serializer\Annotation as JMS;
 
 /**
  */
-class ReaderCreatedEvent implements DomainEventInterface
+class ReaderCreatedEvent implements DomainAggregateEventInterface
 {
     /**
      * @var ReaderId
@@ -72,7 +73,7 @@ class ReaderCreatedEvent implements DomainEventInterface
     /**
      * @return ReaderId
      */
-    public function getId(): ReaderId
+    public function getAggregateId(): AbstractId
     {
         return $this->id;
     }
