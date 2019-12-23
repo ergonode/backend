0a7eed424692deb6eab713568a3889a200a4ccf2
mateusz.kolankowski <wiewiurdp@tlen.pl>
2019-12-23 11:26:43 +0100

    Attribute command tests

--- module/attribute/tests/Domain/Command/UpdateAttributeCommandTest.php
+++ module/attribute/tests/Domain/Command/UpdateAttributeCommandTest.php
@@ -9,6 +9,7 @@ declare(strict_types = 1);
 
 namespace Ergonode\Attribute\Tests\Domain\Command;
 
+use Ergonode\Attribute\Application\Form\Model\AttributeOptionModel;
 use Ergonode\Attribute\Domain\Command\UpdateAttributeCommand;
 use Ergonode\Attribute\Domain\Entity\AttributeId;
 use Ergonode\Core\Domain\ValueObject\TranslatableString;
@@ -45,7 +46,12 @@ class UpdateAttributeCommandTest extends TestCase
         $this->assertSame($placeholder, $command->getPlaceholder());
         $this->assertSame($groups, $command->getGroups());
         $this->assertSame($parameters, $command->getParameters());
-        $this->assertSame($options, $command->getOptions());
+        $this->assertTrue($command->hasParameter('param_1'));
+        $this->assertSame($parameters['param_1'], $command->getParameter('param_1'));
+        $commandOptions = $command->getOptions();
+        $this->assertSame($commandOptions['key_1'], $options[0]->value);
+        $this->assertSame($commandOptions['key_2']->getValue()->getTranslations(), $options[1]->value);
+        $this->assertSame($commandOptions['key_3']->getValue(), $options[2]->value);
     }
 
     /**
@@ -55,6 +61,16 @@ class UpdateAttributeCommandTest extends TestCase
      */
     public function dataProvider(): array
     {
+        $option1 = new AttributeOptionModel();
+        $option1->key = 'key_1';
+        $option1->value = null;
+        $option2 = new AttributeOptionModel();
+        $option2->key = 'key_2';
+        $option2->value = ['EN' => 'english', 'PL' => 'polish'];
+        $option3 = new AttributeOptionModel();
+        $option3->key = 'key_3';
+        $option3->value = 'option';
+
         return [
             [
                 $this->createMock(AttributeId::class),
@@ -62,8 +78,8 @@ class UpdateAttributeCommandTest extends TestCase
                 $this->createMock(TranslatableString::class),
                 $this->createMock(TranslatableString::class),
                 [],
-                [],
-                [],
+                ['param_1' => 'parameter'],
+                [$option1, $option2, $option3],
             ],
         ];
     }
