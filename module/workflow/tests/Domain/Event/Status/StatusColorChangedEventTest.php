1fa038a49b28c18e3971ae6722c91033a0b444ce
Mateusz Kolankowski <wiewiurdp@tlen.pl>
2019-12-24 12:15:19 +0100

    Add workflow event unit tests (#277)

---
+++ module/workflow/tests/Domain/Event/Status/StatusColorChangedEventTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Workflow\Tests\Domain\Event\Status;
+
+use Ergonode\Core\Domain\ValueObject\Color;
+use Ergonode\Workflow\Domain\Entity\StatusId;
+use Ergonode\Workflow\Domain\Event\Status\StatusColorChangedEvent;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+class StatusColorChangedEventTest extends TestCase
+{
+    /**
+     */
+    public function testEventCreation(): void
+    {
+        /** @var StatusId | MockObject $id */
+        $id = $this->createMock(StatusId::class);
+
+        /** @var Color |MockObject $from */
+        $from = $this->createMock(Color::class);
+
+        /** @var Color |MockObject $to */
+        $to = $this->createMock(Color::class);
+
+        $event = new StatusColorChangedEvent($id, $from, $to);
+
+        $this->assertSame($id, $event->getAggregateId());
+        $this->assertSame($from, $event->getFrom());
+        $this->assertSame($to, $event->getTo());
+    }
+}
---
+++ module/workflow/tests/Domain/Event/Workflow/WorkflowDefaultStatusSetEventTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Workflow\Tests\Domain\Event\Workflow;
+
+use Ergonode\Workflow\Domain\Entity\WorkflowId;
+use Ergonode\Workflow\Domain\Event\Workflow\WorkflowDefaultStatusSetEvent;
+use Ergonode\Workflow\Domain\ValueObject\StatusCode;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+class WorkflowDefaultStatusSetEventTest extends TestCase
+{
+    /**
+     */
+    public function testEventCreation(): void
+    {
+        /** @var WorkflowId | MockObject $id */
+        $id = $this->createMock(WorkflowId::class);
+
+        /** @var StatusCode | MockObject $code */
+        $code = $this->createMock(StatusCode::class);
+
+        $event = new WorkflowDefaultStatusSetEvent($id, $code);
+
+        $this->assertSame($id, $event->getAggregateId());
+        $this->assertSame($code, $event->getCode());
+    }
+}
