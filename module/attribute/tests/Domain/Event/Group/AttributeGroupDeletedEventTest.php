27689e39f29cec99357e4e960640de247aaecec5
Mateusz Kolankowski <wiewiurdp@tlen.pl>
2019-12-24 12:16:59 +0100

    Add attribute event unit tests (#275)

---
+++ module/attribute/tests/Domain/Event/Group/AttributeGroupDeletedEventTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Attribute\Tests\Domain\Event\Group;
+
+use Ergonode\Attribute\Domain\Entity\AttributeGroupId;
+use Ergonode\Attribute\Domain\Event\Group\AttributeGroupDeletedEvent;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+class AttributeGroupDeletedEventTest extends TestCase
+{
+    /**
+     */
+    public function testEventCreation(): void
+    {
+        /** @var AttributeGroupId | MockObject $id */
+        $id = $this->createMock(AttributeGroupId::class);
+
+        $event = new AttributeGroupDeletedEvent($id);
+
+        $this->assertSame($id, $event->getAggregateId());
+    }
+}
