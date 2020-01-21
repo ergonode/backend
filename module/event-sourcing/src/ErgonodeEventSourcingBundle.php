f4cfc94ed6cee4c200e5cf24a6b267b07b830ffb
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-01-21 08:24:43 +0100

    messenger_default_bus as parameter and compiler pass for this

--- module/event-sourcing/src/ErgonodeEventSourcingBundle.php
+++ module/event-sourcing/src/ErgonodeEventSourcingBundle.php
@@ -10,9 +10,21 @@ declare(strict_types = 1);
 namespace Ergonode\EventSourcing;
 
 use Ergonode\Core\Application\AbstractModule;
+use Ergonode\EventSourcing\Application\DependencyInjection\Compiler\ResolveMessengerDefaultBusCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  */
 class ErgonodeEventSourcingBundle extends AbstractModule
 {
+    /**
+     * @inheritDoc
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        $container
+            ->addCompilerPass(
+                new ResolveMessengerDefaultBusCompilerPass()
+            );
+    }
 }
