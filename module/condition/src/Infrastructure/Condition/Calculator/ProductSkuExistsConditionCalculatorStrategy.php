8d7bf4c4c5dabe1c5ad264a7cc6b4ff4f915d2aa
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-01-28 11:57:28 +0100

    Add product with sku exists condition (#349)

---
+++ module/condition/src/Infrastructure/Condition/Calculator/ProductSkuExistsConditionCalculatorStrategy.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+
+namespace Ergonode\Condition\Infrastructure\Condition\Calculator;
+
+use Ergonode\Condition\Domain\Condition\ProductSkuExistsCondition;
+use Ergonode\Condition\Domain\ConditionInterface;
+use Ergonode\Condition\Infrastructure\Condition\ConditionCalculatorStrategyInterface;
+use Ergonode\Product\Domain\Entity\AbstractProduct;
+
+/**
+ */
+class ProductSkuExistsConditionCalculatorStrategy implements ConditionCalculatorStrategyInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function supports(string $type): bool
+    {
+        return ProductSkuExistsCondition::TYPE === $type;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function calculate(AbstractProduct $object, ConditionInterface $configuration): bool
+    {
+        $operator = $configuration->getOperator();
+        $pattern = $value = strtolower($configuration->getValue());
+        $sku = strtolower($object->getSku()->getValue());
+
+        switch ($operator) {
+            case ProductSkuExistsCondition::IS_EQUAL:
+                return $value === $sku;
+            case ProductSkuExistsCondition::IS_NOT_EQUAL:
+                return $value != $sku;
+            case ProductSkuExistsCondition::HAS:
+                return strpos($sku, $pattern) !== false;
+            case ProductSkuExistsCondition::WILDCARD:
+                return fnmatch($pattern, $sku) !== false;
+            case ProductSkuExistsCondition::REGEXP:
+                return preg_match($configuration->getValue(), $object->getSku()->getValue()) >= 1;
+        }
+    }
+}
