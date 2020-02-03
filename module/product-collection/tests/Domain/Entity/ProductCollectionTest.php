545044a570d4a543711a46f15b1975e547fa4331
mateusz.kolankowski <wiewiurdp@tlen.pl>
2020-02-03 13:06:53 +0100

    fixing styles....again

--- module/product-collection/tests/Domain/Entity/ProductCollectionTest.php
+++ module/product-collection/tests/Domain/Entity/ProductCollectionTest.php
@@ -78,7 +78,8 @@ class ProductCollectionTest extends TestCase
         $entity->addElement($this->productId, true);
         $this->assertTrue($entity->hasElement($this->productId));
         $this->assertSame($this->productId, $entity->getElement($this->productId)->getProductId());
-        $this->assertSame($this->productId, $entity->getElements()[array_key_first($entity->getElements())]->getProductId());
+        $this->assertSame($this->productId, $entity->getElements()[array_key_first($entity->getElements())]
+        ->getProductId());
         $newName = new TranslatableString(['en' => 'english']);
         $entity->changeName($newName);
         $this->assertEquals($newName, $entity->getName());
