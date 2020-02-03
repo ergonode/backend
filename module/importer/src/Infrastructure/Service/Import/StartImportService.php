b28a04e74640fac1eb657ba9aa616ef6628e6e3a
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2020-02-03 05:44:14 +0100

    clean

--- module/importer/src/Infrastructure/Service/Import/StartImportService.php
+++ module/importer/src/Infrastructure/Service/Import/StartImportService.php
@@ -11,13 +11,15 @@ namespace Ergonode\Importer\Infrastructure\Service\Import;
 use Ergonode\EventSourcing\Infrastructure\Bus\CommandBusInterface;
 use Ergonode\Importer\Domain\Command\Import\ProcessImportCommand;
 use Ergonode\Importer\Domain\Entity\Import;
-use Ergonode\Importer\Domain\Entity\ImportLine;
-use Ergonode\Importer\Domain\Repository\ImportLineRepositoryInterface;
 use Ergonode\Importer\Domain\Repository\SourceRepositoryInterface;
 use Ergonode\ImporterMagento2\Domain\Entity\Magento2CsvSource;
 use Ergonode\Reader\Infrastructure\Provider\ReaderProcessorProvider;
 use Ergonode\Transformer\Infrastructure\Action\ProductImportAction;
 use Webmozart\Assert\Assert;
+use Ergonode\Transformer\Domain\Repository\TransformerRepositoryInterface;
+use Ergonode\Transformer\Domain\Model\Record;
+use Ergonode\Transformer\Infrastructure\Converter\ConverterInterface;
+use Ergonode\Transformer\Infrastructure\Provider\ConverterMapperProvider;
 
 /**
  */
@@ -29,14 +31,20 @@ class StartImportService
     private SourceRepositoryInterface $sourceRepository;
 
     /**
-     * @var ImportLineRepositoryInterface
+     * @var TransformerRepositoryInterface
      */
-    private ImportLineRepositoryInterface $lineRepository;
+    private TransformerRepositoryInterface $transformerRepository;
 
     /**
      * @var ReaderProcessorProvider
      */
     private ReaderProcessorProvider $provider;
+
+    /**
+     * @var ConverterMapperProvider
+     */
+    private ConverterMapperProvider $mapperProvider;
+
 
     /**
      * @var CommandBusInterface
@@ -49,22 +57,27 @@ class StartImportService
     private string $directory;
 
     /**
-     * @param SourceRepositoryInterface     $sourceRepository
-     * @param ImportLineRepositoryInterface $lineRepository
-     * @param ReaderProcessorProvider       $provider
-     * @param CommandBusInterface           $commandBus
-     * @param string                        $directory
+     * StartImportService constructor.
+     *
+     * @param SourceRepositoryInterface $sourceRepository
+     * @param TransformerRepositoryInterface $transformerRepository
+     * @param ReaderProcessorProvider $provider
+     * @param ConverterMapperProvider $mapperProvider
+     * @param CommandBusInterface $commandBus
+     * @param string $directory
      */
     public function __construct(
         SourceRepositoryInterface $sourceRepository,
-        ImportLineRepositoryInterface $lineRepository,
+        TransformerRepositoryInterface $transformerRepository,
         ReaderProcessorProvider $provider,
+        ConverterMapperProvider $mapperProvider,
         CommandBusInterface $commandBus,
         string $directory
     ) {
         $this->sourceRepository = $sourceRepository;
-        $this->lineRepository = $lineRepository;
+        $this->transformerRepository = $transformerRepository;
         $this->provider = $provider;
+        $this->mapperProvider = $mapperProvider;
         $this->commandBus = $commandBus;
         $this->directory = $directory;
     }
@@ -81,6 +94,10 @@ class StartImportService
         $source = $this->sourceRepository->load($import->getSourceId());
         Assert::notNull($source);
 
+        $transformer = $this->transformerRepository->load($import->getTransformerId());
+
+        Assert::notNull($transformer);
+
         $file = $source->getFile();
         $filename = \sprintf('%s%s', $this->directory, $file);
         $extension = pathinfo($filename, PATHINFO_EXTENSION);
@@ -89,9 +106,29 @@ class StartImportService
         $fileReader->open($filename, $source->getConfiguration());
 
         $i = 0;
+        $sku = null;
+        $products = [];
         foreach ($fileReader->read() as $key => $row) {
+            if (!empty($row['sku'])) {
+                $sku = $row['sku'];
+            }
+
+            $result = new Record();
+            foreach ($transformer->getConverters() as $collection => $converters) {
+                /** @var ConverterInterface $converter */
+                foreach ($converters as $field => $converter) {
+                    $mapper = $this->mapperProvider->provide($converter);
+                    $value = $mapper->map($converter, $row);
+                    $result->add($collection, $field, $value);
+                }
+            }
+
+            $products[$sku][] = $row;
+        }
+
+        foreach ($products as $sku => $version) {
             $i++;
-            $command = new ProcessImportCommand($import->getId(), $i, $row, ProductImportAction::TYPE);
+            $command = new ProcessImportCommand($import->getId(), $i, reset($version), ProductImportAction::TYPE);
             $this->commandBus->dispatch($command);
         }
 
