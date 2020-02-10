e327b3171848c5b83aff81e7d91c4a9174faf614
mateusz.kolankowski <wiewiurdp@tlen.pl>
2020-02-10 16:01:27 +0100

    adding product collection system attribute

---
+++ module/product-collection/src/Infrastructure/Grid/Builder/Query/ProductCollectionSystemAttributeDataSetQueryBuilder.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\ProductCollection\Infrastructure\Grid\Builder\Query;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use Ergonode\Attribute\Domain\Entity\AbstractAttribute;
+use Ergonode\Core\Domain\ValueObject\Language;
+use Ergonode\Product\Infrastructure\Grid\Builder\Query\AttributeDataSetQueryBuilderInterface;
+use Ergonode\ProductCollection\Domain\Entity\Attribute\ProductCollectionSystemAttribute;
+
+/**
+ */
+class ProductCollectionSystemAttributeDataSetQueryBuilder implements AttributeDataSetQueryBuilderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function supports(AbstractAttribute $attribute): bool
+    {
+        return $attribute instanceof ProductCollectionSystemAttribute;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addSelect(
+        QueryBuilder $query,
+        string $key,
+        AbstractAttribute $attribute,
+        Language $language
+    ): void {
+        $query->addSelect(
+            sprintf(
+                '(SELECT jsonb_agg(product_collection_id) FROM collection_element ce '.
+                ' WHERE ce.product_id = p.id LIMIT 1) AS "%s"',
+                $key
+            )
+        );
+    }
+}
