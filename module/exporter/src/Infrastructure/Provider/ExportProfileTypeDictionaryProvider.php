060a8b50c51ee47da538c149e44d9b5393d54c61
Wojtek Fajczyk <wojciech.fajczyk@strix.net>
2020-02-10 14:59:36 +0100

    Profile API

---
+++ module/exporter/src/Infrastructure/Provider/ExportProfileTypeDictionaryProvider.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Exporter\Infrastructure\Provider;
+
+use Ergonode\Core\Domain\ValueObject\Language;
+use Ergonode\Exporter\Domain\Factory\ExportProfileFactoryInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ */
+class ExportProfileTypeDictionaryProvider
+{
+    /**
+     * @var TranslatorInterface
+     */
+    private TranslatorInterface $translator;
+
+    /**
+     * @var ExportProfileFactoryInterface ...$exportProfiles
+     */
+    private array $exportProfiles;
+
+    /**
+     * @param TranslatorInterface                   $translator
+     * @param array|ExportProfileFactoryInterface[] $exportProfiles
+     */
+    public function __construct(TranslatorInterface $translator, $exportProfiles)
+    {
+        $this->translator = $translator;
+        $this->exportProfiles = $exportProfiles;
+    }
+
+    /**
+     * @param Language $language
+     *
+     * @return array
+     */
+    public function provide(Language $language): array
+    {
+        $result = [];
+        foreach ($this->exportProfiles as $profile) {
+            $type = $profile->getType();
+            $result[$type] = $this->translator->trans($type, [], 'export_profile', $language->getCode());
+        }
+
+        asort($result);
+
+        return $result;
+    }
+}
