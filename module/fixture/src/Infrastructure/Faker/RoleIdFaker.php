fe73fcec065b7dfca9c7dc082d8afbac965e11b7
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2020-03-16 17:01:38 +0100

    Refactoring role  identifier

--- module/fixture/src/Infrastructure/Faker/RoleIdFaker.php
+++ module/fixture/src/Infrastructure/Faker/RoleIdFaker.php
@@ -11,11 +11,14 @@ namespace Ergonode\Fixture\Infrastructure\Faker;
 
 use Ergonode\SharedKernel\Domain\Aggregate\RoleId;
 use Faker\Provider\Base as BaseProvider;
+use Ramsey\Uuid\Uuid;
 
 /**
  */
 class RoleIdFaker extends BaseProvider
 {
+    private const NAMESPACE = '6601b60b-1701-4db4-87da-944c03aae69f';
+
     /**
      * @param string|null $name
      *
@@ -26,7 +29,7 @@ class RoleIdFaker extends BaseProvider
     public function roleId(?string $name = null): RoleId
     {
         if ($name) {
-            return RoleId::fromString($name);
+            return new RoleId(Uuid::uuid5(self::NAMESPACE, $name)->toString());
         }
 
         return RoleId::generate();
