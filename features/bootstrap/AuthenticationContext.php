911939af94a9d45ba19a79adbaceb3e64498f05e
Daniel Marynicz <daniel.marynicz@gmail.com>
2020-03-16 15:36:51 +0100

    remove Imbo BehatApiExtension and unused code (#430)

--- features/bootstrap/AuthenticationContext.php
+++ features/bootstrap/AuthenticationContext.php
@@ -9,17 +9,11 @@ declare(strict_types = 1);
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  */
 class AuthenticationContext implements Context
 {
-    /**
-     * @var ApiContext
-     */
-    private $apiContext;
-
     /**
      * @var StorageContext
      */
@@ -54,44 +48,9 @@ class AuthenticationContext implements Context
     {
         $environment = $scope->getEnvironment();
 
-        $this->apiContext = $environment->getContext('ApiContext');
         $this->storageContext = $environment->getContext('StorageContext');
 
         $this->storageContext->addDefinition('default_user_username', $this->username);
         $this->storageContext->addDefinition('default_user_password', $this->password);
-    }
-
-    /**
-     * @param string $username
-     * @param string $password
-     *
-     * @Given Authenticate as user :username with password :password
-     */
-    public function login(string $username, string $password): void
-    {
-        $body = json_encode([
-            'username' => $username,
-            'password' => $password,
-        ]);
-
-        $this->apiContext->requestPath('/api/v1/login', Request::METHOD_POST);
-        $this->apiContext->setRequestBody($body);
-        $this->apiContext->requestSend();
-        $this->apiContext->rememberResponseParam('token', 'token');
-    }
-
-    /**
-     * @Given current authentication token
-     */
-    public function authenticationToken(): void
-    {
-        if (!$this->storageContext->has('token')) {
-            $this->login($this->username, $this->password);
-        }
-
-        $this->apiContext->setRequestHeader('JWTAuthorization', sprintf(
-            'Bearer %s',
-            $this->storageContext->get('token')
-        ));
     }
 }
