ecf810c562041a7da67fd7756c7ef2e08f8b8f20
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2020-05-27 11:35:24 +0200

    feature(exporter) add export grid endpoint for given channel (#567)

---
+++ module/channel/src/Application/Controller/Api/Export/ChannelExportGridAction.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Channel\Application\Controller\Api\Export;
+
+use Ergonode\Channel\Domain\Entity\Channel;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Ergonode\Grid\Renderer\GridRenderer;
+use Ergonode\Core\Domain\ValueObject\Language;
+use Ergonode\Grid\RequestGridConfiguration;
+use Ergonode\Api\Application\Response\SuccessResponse;
+use Ergonode\Channel\Infrastructure\Grid\ExportGrid;
+use Ergonode\Channel\Domain\Query\ExportQueryInterface;
+
+/**
+ * @Route(
+ *     name="ergonode_channel_export_grid",
+ *     path="/channels/{channel}/exports",
+ *     methods={"GET"},
+ *     requirements={"channel" = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"}
+ * )
+ */
+class ChannelExportGridAction
+{
+    /**
+     * @var ExportGrid
+     */
+    private ExportGrid $grid;
+
+    /**
+     * @var ExportQueryInterface
+     */
+    private ExportQueryInterface $query;
+
+    /**
+     * @var GridRenderer
+     */
+    private GridRenderer $gridRenderer;
+
+    /**
+     * @param ExportGrid           $grid
+     * @param ExportQueryInterface $query
+     * @param GridRenderer         $gridRenderer
+     */
+    public function __construct(ExportGrid $grid, ExportQueryInterface $query, GridRenderer $gridRenderer)
+    {
+        $this->grid = $grid;
+        $this->query = $query;
+        $this->gridRenderer = $gridRenderer;
+    }
+
+    /**
+     * @IsGranted("CHANNEL_READ")
+     *
+     * @SWG\Tag(name="Channel")
+     * @SWG\Parameter(
+     *     name="language",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     default="en",
+     *     description="Language Code",
+     * )
+     * @SWG\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     type="integer",
+     *     required=true,
+     *     default="50",
+     *     description="Number of returned lines",
+     * )
+     * @SWG\Parameter(
+     *     name="offset",
+     *     in="query",
+     *     type="integer",
+     *     required=true,
+     *     default="0",
+     *     description="Number of start line",
+     * )
+     * @SWG\Parameter(
+     *     name="field",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     description="Order field",
+     * )
+     * @SWG\Parameter(
+     *     name="order",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     enum={"ASC","DESC"},
+     *     description="Order",
+     * )
+     * @SWG\Parameter(
+     *     name="filter",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     description="Filter"
+     * )
+     * @SWG\Parameter(
+     *     name="view",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     enum={"grid","list"},
+     *     description="Specify respons format"
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns export collection",
+     * )
+     *
+     * @ParamConverter(class="Ergonode\Channel\Domain\Entity\Channel")
+     * @ParamConverter(class="Ergonode\Grid\RequestGridConfiguration")
+     *
+     * @param Language                 $language
+     * @param Channel                  $channel
+     * @param RequestGridConfiguration $configuration
+     *
+     * @return Response
+     */
+    public function __invoke(Language $language, Channel $channel, RequestGridConfiguration $configuration): Response
+    {
+        $dataSet = $this->query->getDataSet($channel->getId(), $language);
+
+        $data = $this->gridRenderer->render(
+            $this->grid,
+            $configuration,
+            $dataSet,
+            $language
+        );
+
+        return new SuccessResponse($data);
+    }
+}
---
+++ module/channel/tests/Infrastructure/Grid/ExportGridTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Channel\Tests\Infrastructure\Grid;
+
+use Ergonode\Channel\Infrastructure\Grid\ExportGrid;
+use PHPUnit\Framework\TestCase;
+use Ergonode\Grid\GridConfigurationInterface;
+use Ergonode\Core\Domain\ValueObject\Language;
+
+/**
+ */
+class ExportGridTest extends TestCase
+{
+    /**
+     */
+    public function testGridInit(): void
+    {
+        /** @var GridConfigurationInterface $configuration */
+        $configuration = $this->createMock(GridConfigurationInterface::class);
+        /** @var Language $language */
+        $language = $this->createMock(Language::class);
+        $grid = new ExportGrid();
+        $grid->init($configuration, $language);
+        $this->assertNotEmpty($grid->getColumns());
+    }
+}
