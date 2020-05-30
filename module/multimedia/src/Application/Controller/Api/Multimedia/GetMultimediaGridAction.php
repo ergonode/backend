241f899456aa34b7f4d156cefb4a343f1a5713eb
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2020-05-30 21:34:49 +0200

    feature(multimedia) add multimedia grid (#577)

---
+++ module/multimedia/src/Application/Controller/Api/Multimedia/GetMultimediaGridAction.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Multimedia\Application\Controller\Api\Multimedia;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Ergonode\Core\Domain\ValueObject\Language;
+use Ergonode\Grid\RequestGridConfiguration;
+use Ergonode\Api\Application\Response\SuccessResponse;
+use Ergonode\Grid\Renderer\GridRenderer;
+use Ergonode\Multimedia\Infrastructure\Grid\MultimediaGrid;
+use Ergonode\Multimedia\Domain\Query\MultimediaQueryInterface;
+
+/**
+ * @Route(
+ *     name="ergonode_multimedia_grid",
+ *     path="/{language}/multimedia",
+ *     methods={"GET"},
+ * )
+ */
+class GetMultimediaGridAction
+{
+    /**
+     * @var MultimediaGrid
+     */
+    private MultimediaGrid $grid;
+
+    /**
+     * @var MultimediaQueryInterface
+     */
+    private MultimediaQueryInterface $query;
+
+    /**
+     * @var GridRenderer
+     */
+    private GridRenderer $renderer;
+
+    /**
+     * @param MultimediaGrid           $grid
+     * @param MultimediaQueryInterface $query
+     * @param GridRenderer             $renderer
+     */
+    public function __construct(MultimediaGrid $grid, MultimediaQueryInterface $query, GridRenderer $renderer)
+    {
+        $this->grid = $grid;
+        $this->query = $query;
+        $this->renderer = $renderer;
+    }
+
+    /**
+     * @SWG\Tag(name="Multimedia")
+     * @SWG\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     type="integer",
+     *     required=true,
+     *     default="50",
+     *     description="Number of returned lines",
+     * )
+     * @SWG\Parameter(
+     *     name="offset",
+     *     in="query",
+     *     type="integer",
+     *     required=true,
+     *     default="0",
+     *     description="Number of start line",
+     * )
+     * @SWG\Parameter(
+     *     name="field",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     description="Order field",
+     * )
+     * @SWG\Parameter(
+     *     name="filter",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     description="Filter"
+     * )
+     * @SWG\Parameter(
+     *     name="order",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     enum={"ASC","DESC"},
+     *     description="Order",
+     * )
+     * @SWG\Parameter(
+     *     name="view",
+     *     in="query",
+     *     required=false,
+     *     type="string",
+     *     enum={"grid","list"},
+     *     description="Specify respons format"
+     * )
+     * @SWG\Parameter(
+     *     name="language",
+     *     in="path",
+     *     type="string",
+     *     required=true,
+     *     default="en",
+     *     description="Language Code",
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns multimedia ggrid",
+     * )
+     *
+     * @ParamConverter(class="Ergonode\Grid\RequestGridConfiguration")
+     *
+     * @param Language                 $language
+     * @param RequestGridConfiguration $configuration
+     *
+     * @return Response
+     */
+    public function __invoke(Language $language, RequestGridConfiguration $configuration): Response
+    {
+        $dataSet = $this->query->getDataSet();
+
+        $data = $this->renderer->render(
+            $this->grid,
+            $configuration,
+            $dataSet,
+            $language
+        );
+
+        return new SuccessResponse($data);
+    }
+}
