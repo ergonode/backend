d9d36b812f7fde67faddd7eeef085e1113053c92
mateusz.kolankowski <wiewiurdp@tlen.pl>
2020-07-02 00:11:11 +0200

    feature(account) small fixes

--- module/account/src/Application/Controller/Api/Account/AvatarChangeAction.php
+++ module/account/src/Application/Controller/Api/Account/AvatarChangeAction.php
@@ -29,6 +29,7 @@ use Ergonode\EventSourcing\Infrastructure\Bus\CommandBusInterface;
  *     name="ergonode_avatar_change",
  *     path="/accounts/{user}/avatar",
  *     methods={"PUT"},
+ *     requirements={"user"="[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"},
  *     )
  */
 class AvatarChangeAction
