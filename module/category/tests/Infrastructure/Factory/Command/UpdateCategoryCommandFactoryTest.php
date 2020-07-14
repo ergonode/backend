b430dd90f6b3ca315614af9232e955dbc822c144
mateusz.kolankowski <wiewiurdp@tlen.pl>
2020-07-14 11:00:18 +0200

    tests(category) Unit tests for category module

---
+++ module/category/tests/Infrastructure/Factory/Command/UpdateCategoryCommandFactoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Category\Tests\Infrastructure\Factory\Command;
+
+use Ergonode\Category\Application\Model\CategoryFormModel;
+use Ergonode\Category\Domain\Command\UpdateCategoryCommand;
+use Ergonode\Category\Domain\Entity\Category;
+use Ergonode\Category\Domain\ValueObject\CategoryCode;
+use Ergonode\Category\Infrastructure\Factory\Command\UpdateCategoryCommandFactory;
+use Ergonode\Core\Domain\ValueObject\TranslatableString;
+use Ergonode\SharedKernel\Domain\Aggregate\CategoryId;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Form\FormInterface;
+
+/**
+ */
+class UpdateCategoryCommandFactoryTest extends TestCase
+{
+    /**
+     */
+    public function testSupported(): void
+    {
+        $commandFactory = new UpdateCategoryCommandFactory();
+        $this->assertTrue($commandFactory->support(Category::TYPE));
+        $this->assertFalse($commandFactory->support('Any other type'));
+    }
+
+    /**
+     */
+    public function testCreation(): void
+    {
+        $name = ['pl_PL' => 'Any Name'];
+        $data = $this->createMock(CategoryFormModel::class);
+        $data->name = $name;
+
+        $form = $this->createMock(FormInterface::class);
+        $form->method('getData')->willReturn($data);
+
+        $commandFactory = new UpdateCategoryCommandFactory();
+        $categorId = $this->createMock(CategoryId::class);
+
+        /** @var UpdateCategoryCommand $command */
+        $command = $commandFactory->create($categorId, $form);
+        $this->assertEquals($command->getName(), new TranslatableString($name));
+        $this->assertEquals($command->getId(), $categorId);
+    }
+}
---
+++ module/category/tests/Infrastructure/Provider/CreateCategoryCommandFactoryProviderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Category\Tests\Infrastructure\Provider;
+
+use Ergonode\Category\Infrastructure\Factory\Command\CreateCategoryCommandFactoryInterface;
+use Ergonode\Category\Infrastructure\Provider\CreateCategoryCommandFactoryProvider;
+use PHPUnit\Framework\TestCase;
+
+/**
+ */
+class CreateCategoryCommandFactoryProviderTest extends TestCase
+{
+    /**
+     */
+    public function testFactoryProvide(): void
+    {
+        $factory = $this->createMock(CreateCategoryCommandFactoryInterface::class);
+        $factory->method('support')->willReturn(true);
+        $provider = new CreateCategoryCommandFactoryProvider(...[$factory]);
+
+        $factoryProvided = $provider->provide('type');
+        $this->assertEquals($factory, $factoryProvided);
+    }
+
+    /**
+     */
+    public function testNotExistingFactory(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $factory = $this->createMock(CreateCategoryCommandFactoryInterface::class);
+        $factory->method('support')->willReturn(false);
+        $provider = new CreateCategoryCommandFactoryProvider(...[$factory]);
+
+        $factoryProvided = $provider->provide('type');
+    }
+}
