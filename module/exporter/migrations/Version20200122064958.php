013ae6b6d19ad04b0f8be56cf415a4030bde54ac
Wojciech Fajczyk <w.f.wojtek@gmail.com>
2020-08-21 10:06:58 +0200

    bugfix(account) hidden role

--- module/exporter/migrations/Version20200122064958.php
+++ module/exporter/migrations/Version20200122064958.php
@@ -84,16 +84,6 @@ final class Version20200122064958 extends AbstractErgonodeMigration
             )
         ');
 
-        $this->addSql('
-            CREATE TABLE exporter.channel_configuration (
-                id uuid NOT NULL,
-                "type" VARCHAR(255) NOT NULL,
-                "configuration" json NOT NULL,
-                created_at timestamp NOT NULL,
-                updated_at timestamp NOT NULL,
-                PRIMARY KEY (id)
-                )
-        ');
     }
 
     /**
