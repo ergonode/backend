22d6ae9a156ef3b1544a932bbb1b7236d5d1a80b
mateusz.kolankowski <wiewiurdp@tlen.pl>
2020-09-03 14:15:18 +0200

    bug(account) Fixing not working privileges on backend

--- module/account/src/Application/Security/Voter/UserRoleVoter.php
+++ module/account/src/Application/Security/Voter/UserRoleVoter.php
@@ -52,8 +52,7 @@ class UserRoleVoter extends Voter implements LoggerAwareInterface
     public function supports($attribute, $subject): bool
     {
         $privileges = $this->query->getPrivileges();
-
-        return in_array($attribute, $privileges, true);
+        return in_array($attribute, array_column($privileges, "code"), true);
     }
 
     /**
