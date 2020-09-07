6f1c073ef01fb09af6a78bb19f33ec86a6dc250f
mateusz.kolankowski <wiewiurdp@tlen.pl>
2020-09-07 11:31:49 +0200

    bug(designer) fixes after CR

--- module/core/src/Application/Controller/Api/LanguageTree/LanguageTreeReadAction.php
+++ module/core/src/Application/Controller/Api/LanguageTree/LanguageTreeReadAction.php
@@ -11,7 +11,6 @@ namespace Ergonode\Core\Application\Controller\Api\LanguageTree;
 use Ergonode\Api\Application\Response\SuccessResponse;
 use Ergonode\Core\Domain\Repository\LanguageTreeRepositoryInterface;
 use Ergonode\Core\Domain\ValueObject\Language;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
