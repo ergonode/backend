8464705a3b255bff27ea2fc3256da8f75afbc84f
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2020-10-01 12:43:59 +0200

    feature(authentication) Storing id in JWT instead of email address (#806)

---
+++ module/authentication/src/Application/Security/Provider/EmailUserProvider.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Authentication\Application\Security\Provider;
+
+use Ergonode\Account\Domain\Query\UserQueryInterface;
+use Ergonode\Account\Domain\Repository\UserRepositoryInterface;
+use Ergonode\Authentication\Application\Security\User\User;
+use Ergonode\SharedKernel\Domain\Exception\InvalidEmailException;
+use Ergonode\SharedKernel\Domain\ValueObject\Email;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+/**
+ */
+final class EmailUserProvider implements UserProviderInterface
+{
+    private UserQueryInterface $query;
+    private UserRepositoryInterface $repository;
+
+    /**
+     * @param UserQueryInterface      $query
+     * @param UserRepositoryInterface $repository
+     */
+    public function __construct(UserQueryInterface $query, UserRepositoryInterface $repository)
+    {
+        $this->query = $query;
+        $this->repository = $repository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadUserByUsername($username): User
+    {
+        if (empty($username)) {
+            throw new UsernameNotFoundException('Empty username');
+        }
+        if (!is_string($username)) {
+            throw new UsernameNotFoundException('Username has to be a string');
+        }
+
+        try {
+            $email = new Email($username);
+        } catch (InvalidEmailException $exception) {
+            throw new UsernameNotFoundException('Invalid email format');
+        }
+
+        $userId = $this->query->findIdByEmail($email);
+        if (!$userId || !$user = $this->repository->load($userId)) {
+            throw new UsernameNotFoundException("Username '$username' not found");
+        }
+
+        return new User(
+            $user->getId()->getValue(),
+            $user->getPassword(),
+            $user->getRoles(),
+            $user->isActive(),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function refreshUser(UserInterface $user): User
+    {
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsClass($class): bool
+    {
+        return User::class === $class;
+    }
+}
---
+++ module/authentication/tests/Application/Security/Provider/EmailUserProviderTest.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Authentication\Tests\Application\Security\Provider;
+
+use Ergonode\Account\Domain\Entity\User;
+use Ergonode\Account\Domain\Query\UserQueryInterface;
+use Ergonode\Account\Domain\Repository\UserRepositoryInterface;
+use Ergonode\Authentication\Application\Security\Provider\EmailUserProvider;
+use Ergonode\Authentication\Application\Security\User\User as SecurityUser;
+use Ergonode\SharedKernel\Domain\Aggregate\UserId;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+/**
+ */
+class EmailUserProviderTest extends TestCase
+{
+    /**
+     * @var UserQueryInterface|MockObject
+     */
+    private $mockUserQuery;
+    /**
+     * @var UserRepositoryInterface|MockObject
+     */
+    private $mockUserRepository;
+    private EmailUserProvider $provider;
+
+    /**
+     */
+    protected function setUp(): void
+    {
+        $this->mockUserQuery = $this->createMock(UserQueryInterface::class);
+        $this->mockUserRepository = $this->createMock(UserRepositoryInterface::class);
+
+        $this->provider = new EmailUserProvider(
+            $this->mockUserQuery,
+            $this->mockUserRepository,
+        );
+    }
+
+    /**
+     */
+    public function testShouldProvide(): void
+    {
+        $user = $this->createMock(User::class);
+        $user
+            ->method('getId')
+            ->willReturn($userId = new UserId((string) Uuid::uuid4()));
+        $user
+            ->method('getPassword')
+            ->willReturn('password');
+        $user
+            ->method('getRoles')
+            ->willReturn(['roles']);
+        $user
+            ->method('isActive')
+            ->willReturn(true);
+        $this->mockUserQuery
+            ->method('findIdByEmail')
+            ->willReturn(new UserId((string) Uuid::uuid4()));
+        $this->mockUserRepository
+            ->method('load')
+            ->willReturn($user);
+
+        $result = $this->provider->loadUserByUsername('test@ergonode.com');
+
+        $this->assertEquals(
+            new SecurityUser(
+                $userId->getValue(),
+                'password',
+                ['roles'],
+                true,
+            ),
+            $result,
+        );
+    }
+
+    /**
+     */
+    public function testThrowExceptionWhenNoSuchUser(): void
+    {
+        $this->mockUserQuery
+            ->method('findIdByEmail')
+            ->willReturn(new UserId((string) Uuid::uuid4()));
+        $this->mockUserRepository
+            ->method('load')
+            ->willReturn(null);
+
+        $this->expectExceptionMessage('Username \'test@ergonode.com\' not found');
+
+        $this->provider->loadUserByUsername('test@ergonode.com');
+    }
+
+    /**
+     */
+    public function testThrowExceptionWhenNoSuchUserName(): void
+    {
+        $this->mockUserQuery
+            ->method('findIdByEmail')
+            ->willReturn(null);
+        $this->mockUserRepository
+            ->expects($this->never())
+            ->method('load');
+
+        $this->expectExceptionMessage('Username \'test@ergonode.com\' not found');
+
+        $this->provider->loadUserByUsername('test@ergonode.com');
+    }
+
+    /**
+     */
+    public function testThrowExceptionWhenInvalidEmailGiven(): void
+    {
+        $this->expectExceptionMessage('Invalid email format');
+
+        $this->provider->loadUserByUsername('plain string');
+    }
+
+    /**
+     */
+    public function testThrowExceptionWhenNoStringGiven(): void
+    {
+        $this->expectExceptionMessage('Username has to be a string');
+
+        $this->provider->loadUserByUsername(1);
+    }
+
+    /**
+     */
+    public function testThrowExceptionWhenEmptyStringGiven(): void
+    {
+        $this->expectExceptionMessage('Empty username');
+
+        $this->provider->loadUserByUsername('');
+    }
+}
