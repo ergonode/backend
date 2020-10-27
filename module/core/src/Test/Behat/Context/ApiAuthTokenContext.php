01bafeb1fa478db80114b160f2b38660dd56db46
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2020-10-27 15:25:31 +0100

    feature(authentication) add refresh token (#883)

---
+++ module/core/src/Test/Behat/Context/ApiAuthTokenContext.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Core\Test\Behat\Context;
+
+use Behat\Behat\Context\Context;
+use Behatch\HttpCall\Request;
+
+class ApiAuthTokenContext implements Context
+{
+    private Request $request;
+
+    private string $authHeader;
+
+    public function __construct(Request $request, string $authHeader)
+    {
+        $this->request = $request;
+        $this->authHeader = $authHeader;
+    }
+
+    /**
+     * @Given I use Authenticated token :token
+     */
+    public function iAmAuthenticatedAsToken(string $token): void
+    {
+        $this->request->setHttpHeader($this->authHeader, 'Bearer '.$token);
+    }
+}
