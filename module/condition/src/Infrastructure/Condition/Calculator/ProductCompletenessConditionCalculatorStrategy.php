c0ad68a4b333bce1ec673f8772d826b2bb524a3e
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2020-10-30 08:13:21 +0100

    bugfix(condition) calculate completanse strategy (#895)

--- module/condition/src/Infrastructure/Condition/Calculator/ProductCompletenessConditionCalculatorStrategy.php
+++ module/condition/src/Infrastructure/Condition/Calculator/ProductCompletenessConditionCalculatorStrategy.php
@@ -9,36 +9,25 @@ declare(strict_types=1);
 
 namespace Ergonode\Condition\Infrastructure\Condition\Calculator;
 
-use Ergonode\Completeness\Domain\Calculator\CompletenessCalculator;
+use Ergonode\Completeness\Infrastructure\Persistence\Query\CompletenessQuery;
 use Ergonode\Condition\Domain\Condition\ProductCompletenessCondition;
 use Ergonode\Condition\Domain\ConditionInterface;
 use Ergonode\Condition\Infrastructure\Condition\ConditionCalculatorStrategyInterface;
 use Ergonode\Core\Domain\Query\LanguageQueryInterface;
-use Ergonode\Designer\Domain\Repository\TemplateRepositoryInterface;
-use Ergonode\Editor\Domain\Provider\DraftProvider;
 use Ergonode\Product\Domain\Entity\AbstractProduct;
-use Webmozart\Assert\Assert;
 
 class ProductCompletenessConditionCalculatorStrategy implements ConditionCalculatorStrategyInterface
 {
-    private CompletenessCalculator $calculator;
-
-    private TemplateRepositoryInterface $repository;
-
     private LanguageQueryInterface $query;
 
-    private DraftProvider $provider;
+    private CompletenessQuery $completenessQuery;
 
     public function __construct(
-        CompletenessCalculator $calculator,
-        TemplateRepositoryInterface $repository,
         LanguageQueryInterface $query,
-        DraftProvider $provider
+        CompletenessQuery $completenessQuery
     ) {
-        $this->calculator = $calculator;
-        $this->repository = $repository;
         $this->query = $query;
-        $this->provider = $provider;
+        $this->completenessQuery = $completenessQuery;
     }
 
     /**
@@ -52,22 +41,14 @@ class ProductCompletenessConditionCalculatorStrategy implements ConditionCalcula
     /**
      * @param ConditionInterface|ProductCompletenessCondition $configuration
      *
-     *
      * @throws \Exception
      */
     public function calculate(AbstractProduct $object, ConditionInterface $configuration): bool
     {
-        $draft = $this->provider->provide($object);
-
-        $templateId = $object->getTemplateId();
-
-        $template = $this->repository->load($templateId);
-        Assert::notNull($template, sprintf('Can\'t find template %s', $templateId->getValue()));
-
         $result = true;
 
         foreach ($this->query->getActive() as $code) {
-            $calculation = $this->calculator->calculate($draft, $template, $code);
+            $calculation = $this->completenessQuery->getCompleteness($object->getId(), $code);
             if ($configuration->getCompleteness() === ProductCompletenessCondition::COMPLETE) {
                 if ($calculation->getPercent() < 100) {
                     $result = false;
