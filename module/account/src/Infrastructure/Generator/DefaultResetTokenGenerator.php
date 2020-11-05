6bb1afa8029956d057e58f4728d1e0692c423330
Wojciech Fajczyk <w.f.wojtek@gmail.com>
2020-11-05 10:27:32 +0100

    feature(account)add reset token

---
+++ module/account/src/Infrastructure/Generator/DefaultResetTokenGenerator.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Account\Infrastructure\Generator;
+
+use Ergonode\Account\Domain\ValueObject\ResetToken;
+
+class DefaultResetTokenGenerator implements ResetTokenGeneratorInterface
+{
+    /**
+     * @throws \Exception
+     */
+    public function getToken(): ResetToken
+    {
+        return new ResetToken(bin2hex(random_bytes(64)));
+    }
+}
---
+++ module/account/src/Infrastructure/Generator/ResetTokenGeneratorInterface.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Account\Infrastructure\Generator;
+
+use Ergonode\Account\Domain\ValueObject\ResetToken;
+
+interface ResetTokenGeneratorInterface
+{
+    public function getToken(): ResetToken;
+}
