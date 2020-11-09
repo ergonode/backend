08145c814a4ce46d4ced748c7e9c24d7d7cce9b7
Sebastian <bastek.bielawski@gmail.com>
2020-11-09 18:58:24 +0100

    refactoring(all): Remove CPD and MD

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
