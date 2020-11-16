35adaacec9d41379e5ba221e109c7b7a85af45c7
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2020-11-16 13:33:30 +0100

    feature(draft) remove unnecesery classes

---
+++ module/completeness/src/Infrastructure/Handler/Event/ProductValueRemovedEventHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Completeness\Infrastructure\Handler\Event;
+
+use Ergonode\EventSourcing\Infrastructure\Bus\CommandBusInterface;
+use Ergonode\Completeness\Domain\Command\ProductCompletenessCalculateCommand;
+use Ergonode\Product\Domain\Event\ProductValueRemovedEvent;
+
+class ProductValueRemovedEventHandler
+{
+    private CommandBusInterface $commandBus;
+
+    public function __construct(CommandBusInterface $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function __invoke(ProductValueRemovedEvent $event): void
+    {
+        $command = new ProductCompletenessCalculateCommand($event->getAggregateId());
+        $this->commandBus->dispatch($command, true);
+    }
+}
---
+++ module/designer/migrations/Version20201116125500.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20201116125500 extends AbstractErgonodeMigration
+{
+    /**
+     * @throws \Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE designer.product');
+    }
+}
