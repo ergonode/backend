6814a3cf890ee012d0db3cb80e7ad3619b5f4ee0
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2020-11-27 10:26:39 +0100

    feature(products) add missing validators

--- module/product/src/Application/Model/Product/Attribute/Delete/DeleteProductAttributeFormModel.php
+++ module/product/src/Application/Model/Product/Attribute/Delete/DeleteProductAttributeFormModel.php
@@ -9,12 +9,14 @@ declare(strict_types=1);
 namespace Ergonode\Product\Application\Model\Product\Attribute\Delete;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Ergonode\Product\Infrastructure\Validator\ProductExists;
 
 class DeleteProductAttributeFormModel
 {
     /**
      * @Assert\NotBlank()
      * @Assert\Uuid(strict=true)
+     * @ProductExists()
      */
     public ?string $id = null;
 
