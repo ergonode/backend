40838ae9e6d4a0d4ea99801569afb5ccc124bc4b
Wojciech Fajczyk <w.f.wojtek@gmail.com>
2020-12-01 14:51:14 +0100

    bugfix(multimedia) count relations

--- module/multimedia/src/Infrastructure/Persistence/Query/DbalMultimediaQuery.php
+++ module/multimedia/src/Infrastructure/Persistence/Query/DbalMultimediaQuery.php
@@ -87,16 +87,10 @@ class DbalMultimediaQuery implements MultimediaQueryInterface
             ->addSelect('(left(m.mime, strpos(m.mime, \'/\')-1)) AS type')
             ->addSelect('(m.size / 1024.00)::NUMERIC(10,2) AS size')
             ->addSelect('m.id AS image')
-            ->addSelect('(SELECT sum(calc.count) FROM (
-                                    SELECT count(*) FROM product_value pv
-                                    JOIN Value_translation vt ON vt.value_id = pv.value_id
-                                    WHERE vt.value = m.id::TEXT
-                                UNION
-                                    SELECT count(*) FROM designer."template" te
-                                    WHERE te.image_id = m.id
-                                ) AS calc
+            ->addSelect('(SELECT COUNT(DISTINCT pv.product_id) FROM product_value pv
+                                    JOIN value_translation vt ON vt.value_id = pv.value_id
+                                    WHERE vt.value ILIKE CONCAT(\'%\', m.id::TEXT, \'%\')
                                 ) AS relations');
-
         $result = $this->connection->createQueryBuilder();
         $result->select('*');
         $result->from(sprintf('(%s)', $qb->getSQL()), 't');
