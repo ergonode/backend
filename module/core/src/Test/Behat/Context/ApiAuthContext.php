baeed23af8781efcfae6fe0d23bf7ef91908962b
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2020-12-01 10:50:46 +0100

    bugfix(core) Resolved authentication and core modules dependencies (#997)

--- module/core/src/Test/Behat/Context/ApiAuthContext.php
+++ module/core/src/Test/Behat/Context/ApiAuthContext.php
@@ -11,30 +11,25 @@ namespace Ergonode\Core\Test\Behat\Context;
 
 use Behat\Behat\Context\Context;
 use Behatch\HttpCall\Request;
-use Ergonode\Authentication\Application\Security\User\User;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Ergonode\Core\Test\Behat\Service\RequestAuthenticatorInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ApiAuthContext implements Context
 {
-    private JWTTokenManagerInterface $JWTTokenManager;
-
     private Request $request;
+    private RequestAuthenticatorInterface $authenticator;
 
-    public function __construct(
-        JWTTokenManagerInterface $JWTTokenManager,
-        Request $request
-    ) {
-        $this->JWTTokenManager = $JWTTokenManager;
-        $this->request         = $request;
+    public function __construct(Request $request, RequestAuthenticatorInterface $authenticator)
+    {
+        $this->request = $request;
+        $this->authenticator = $authenticator;
     }
 
     /**
      * @Given I am Authenticated as :user
      */
-    public function iAmAuthenticatedAsUser(User $user): void
+    public function iAmAuthenticatedAsUser(UserInterface $user): void
     {
-        $token = $this->JWTTokenManager->create($user);
-        /** @phpstan-ignore-next-line */
-        $this->request->setHttpHeader('JWTAuthorization', 'Bearer '.$token);
+        $this->authenticator->authenticate($this->request, $user);
     }
 }
