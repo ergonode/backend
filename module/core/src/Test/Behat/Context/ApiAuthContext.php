0fa5d22942d9b2db1fd3d2db4f903884de5be209
Piotr Kreft <kreftpiotrek@gmail.com>
2020-12-01 09:23:25 +0100

    bugfix(core) Resolved authentication and core modules dependencies

--- module/core/src/Test/Behat/Context/ApiAuthContext.php
+++ module/core/src/Test/Behat/Context/ApiAuthContext.php
@@ -11,30 +11,19 @@ namespace Ergonode\Core\Test\Behat\Context;
 
 use Behat\Behat\Context\Context;
 use Behatch\HttpCall\Request;
-use Ergonode\Authentication\Application\Security\User\User;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Ergonode\Core\Test\Behat\Service\RequestAuthenticatorInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ApiAuthContext implements Context
 {
-    private JWTTokenManagerInterface $JWTTokenManager;
-
     private Request $request;
-
-    public function __construct(
-        JWTTokenManagerInterface $JWTTokenManager,
-        Request $request
-    ) {
-        $this->JWTTokenManager = $JWTTokenManager;
-        $this->request         = $request;
-    }
+    private RequestAuthenticatorInterface $authenticator;
 
     /**
      * @Given I am Authenticated as :user
      */
-    public function iAmAuthenticatedAsUser(User $user): void
+    public function iAmAuthenticatedAsUser(UserInterface $user): void
     {
-        $token = $this->JWTTokenManager->create($user);
-        /** @phpstan-ignore-next-line */
-        $this->request->setHttpHeader('JWTAuthorization', 'Bearer '.$token);
+        $this->authenticator->authenticate($this->request, $user);
     }
 }
---
+++ module/core/src/Test/Behat/Service/RequestAuthenticatorInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Core\Test\Behat\Service;
+
+use Behatch\HttpCall\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+interface RequestAuthenticatorInterface
+{
+    public function authenticate(Request $request, UserInterface $user): void;
+}
