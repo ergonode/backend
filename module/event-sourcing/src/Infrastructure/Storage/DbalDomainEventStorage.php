331a9fe7fefdd8f2cf89f63c539a4e32c75b9cda
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2020-12-01 11:01:29 +0100

    feature(event-sourcing) Calculation of event sequence on DB (#992)

--- module/event-sourcing/src/Infrastructure/Storage/DbalDomainEventStorage.php
+++ module/event-sourcing/src/Infrastructure/Storage/DbalDomainEventStorage.php
@@ -75,9 +75,13 @@ class DbalDomainEventStorage implements DomainEventStorageInterface
     /**
      * @throws \Throwable
      */
-    public function append(AggregateId $id, DomainEventStream $stream, string $name = null): void
+    public function append(AggregateId $id, DomainEventStream $stream, string $name = null): int
     {
-        $this->connection->transactional(function () use ($id, $stream, $name): void {
+        if (0 === $stream->count()) {
+            throw new \DomainException('Cannot append empty events stream.');
+        }
+
+        return $this->connection->transactional(function () use ($id, $stream, $name): int {
             $table = $name ?: self::TABLE;
             $token = $this->tokenStorage->getToken();
             $userId = null;
@@ -86,23 +90,40 @@ class DbalDomainEventStorage implements DomainEventStorageInterface
                 $user = $token->getUser();
                 $userId = $user->getId()->getValue();
             }
+            $sql = "
+                INSERT INTO $table (aggregate_id, sequence, event_id, payload, recorded_at, recorded_by)
+                    VALUES (
+                        :aggregateId,
+                         COALESCE(
+                            (
+                                (SELECT sequence FROM event_store WHERE aggregate_id = :aggregateId
+                                    ORDER BY sequence DESC LIMIT 1
+                                ) + 1
+                            ),
+                            1
+                        ),
+                        :eventId,
+                        :payload,
+                        :recorderAt,
+                        :recordedBy
+                    )
+                    RETURNING sequence
+                ";
             foreach ($stream as $envelope) {
                 $payload = $this->serializer->serialize($envelope->getEvent(), 'json');
-                $this->connection->insert(
-                    $table,
-                    [
-                        'aggregate_id' => $id->getValue(),
-                        'sequence' => $envelope->getSequence(),
-                        'event_id' => $this->domainEventProvider->provideEventId($envelope->getType()),
-                        'payload' => $payload,
-                        'recorded_at' => $envelope->getRecordedAt(),
-                        'recorded_by' => $userId,
-                    ],
-                    [
-                        'recorded_at' => Types::DATETIMETZ_MUTABLE,
-                    ],
-                );
+                $stmt = $this->connection->prepare($sql);
+
+                $stmt->bindValue('aggregateId', $id->getValue());
+                $stmt->bindValue('eventId', $this->domainEventProvider->provideEventId($envelope->getType()));
+                $stmt->bindValue('payload', $payload);
+                $stmt->bindValue('recorderAt', $envelope->getRecordedAt(), Types::DATETIMETZ_MUTABLE);
+                $stmt->bindValue('recordedBy', $userId);
+
+                $stmt->execute();
+                $sequence = $stmt->fetchColumn();
             }
+
+            return $sequence; /** @phpstan-ignore-line */
         });
     }
 
