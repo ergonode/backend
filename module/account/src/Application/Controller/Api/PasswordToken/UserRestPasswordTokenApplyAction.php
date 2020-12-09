8804569097866675952519641609146fd665adc3
Wojciech Fajczyk <w.f.wojtek@gmail.com>
2020-12-09 11:13:34 +0100

    feature(accont) token apply

---
+++ module/account/src/Application/Controller/Api/PasswordToken/UserRestPasswordTokenApplyAction.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Account\Application\Controller\Api\PasswordToken;
+
+use Ergonode\Account\Application\Form\Model\UserApplyTokenModel;
+use Ergonode\Account\Application\Form\UserApplyTokenForm;
+use Ergonode\Account\Domain\Command\User\ApplyUserResetTokenCommand;
+use Ergonode\Account\Domain\ValueObject\Password;
+use Ergonode\Account\Domain\ValueObject\ResetToken;
+use Ergonode\Api\Application\Exception\FormValidationHttpException;
+use Ergonode\Api\Application\Response\EmptyResponse;
+use Swagger\Annotations as SWG;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Ergonode\EventSourcing\Infrastructure\Bus\CommandBusInterface;
+
+/**
+ * @Route(
+ *     name="ergonode_reset_token_apply",
+ *     path="accounts/token/apply",
+ *     methods={"POST"},
+ *     )
+ */
+class UserRestPasswordTokenApplyAction
+{
+    private FormFactoryInterface $formFactory;
+
+    private CommandBusInterface $commandBus;
+
+    public function __construct(FormFactoryInterface $formFactory, CommandBusInterface $commandBus)
+    {
+        $this->formFactory = $formFactory;
+        $this->commandBus = $commandBus;
+    }
+
+    /**
+     * @SWG\Tag(name="Account")
+     *
+     * @SWG\Parameter(
+     *     name="body",
+     *     in="body",
+     *     description="User email",
+     *     required=true,
+     *     @SWG\Schema(ref="#/definitions/account_token_apply")
+     * )
+     * @SWG\Response(
+     *     response=204,
+     *     description="Success"
+     * )
+     * @SWG\Response(
+     *     response=400,
+     *     description="Validation error",
+     *     @SWG\Schema(ref="#/definitions/validation_error_response")
+     * )
+     */
+    public function __invoke(Request $request): Response
+    {
+        $model = new UserApplyTokenModel();
+        $form = $this->formFactory->create(UserApplyTokenForm::class, $model);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UserApplyTokenModel $data */
+            $data = $form->getData();
+            $command = new ApplyUserResetTokenCommand(
+                new ResetToken($data->token),
+                new Password($data->password)
+            );
+            $this->commandBus->dispatch($command);
+
+            return new EmptyResponse();
+        }
+        throw new FormValidationHttpException($form);
+    }
+}
---
+++ module/account/src/Infrastructure/Handler/User/ApplyUserResetTokenCommandHandler.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Account\Infrastructure\Handler\User;
+
+use Ergonode\Account\Domain\Command\User\ApplyUserResetTokenCommand;
+use Ergonode\Account\Domain\Repository\UserRepositoryInterface;
+use Ergonode\Account\Domain\Repository\UserResetPasswordTokenRepositoryInterface;
+use Ergonode\Account\Infrastructure\Encoder\UserPasswordEncoderInterface;
+use Webmozart\Assert\Assert;
+
+class ApplyUserResetTokenCommandHandler
+{
+    private UserResetPasswordTokenRepositoryInterface $tokenRepository;
+
+    private UserRepositoryInterface $userRepository;
+
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+
+    public function __construct(
+        UserResetPasswordTokenRepositoryInterface $tokenRepository,
+        UserRepositoryInterface $userRepository,
+        UserPasswordEncoderInterface $userPasswordEncoder
+    ) {
+        $this->tokenRepository = $tokenRepository;
+        $this->userRepository = $userRepository;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    public function __invoke(ApplyUserResetTokenCommand $command): void
+    {
+        $token = $this->tokenRepository->load($command->getToken());
+        Assert::notNull($token);
+
+        $user = $this->userRepository->load($token->getUserId());
+        Assert::notNull($user);
+
+        $encodedPassword = $this->userPasswordEncoder->encode($user, $command->getPassword());
+        if ($user->getPassword() !== $encodedPassword->getValue()) {
+            $user->changePassword($encodedPassword);
+        }
+        $token->setConsumed(new \DateTime());
+
+        $this->userRepository->save($user);
+        $this->tokenRepository->save($token);
+    }
+}
