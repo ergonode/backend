a376177373b8c896be0e17ae339b14eebcd1d331
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2020-12-15 12:35:28 +0100

    refactor(event sourcing) Configurable snapshot frequency value (#1027)

---
+++ module/event-sourcing/src/Application/DependencyInjection/Configuration.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\EventSourcing\Application\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('ergonode_event_sourcing');
+
+        /** @phpstan-ignore-next-line */
+        $treeBuilder
+            ->getRootNode()
+                ->children()
+                    ->integerNode('snapshot_frequency')
+                        ->defaultValue(10)
+                        ->validate()
+                            ->ifTrue(fn (int $snapshotFrequency): bool => $snapshotFrequency <= 0)
+                            ->thenInvalid('Snapshot frequency has to be positive integer.')
+                        ->end()
+                        ->info(
+                            'Indicates how frequently snapshots of aggregate (every X events) are taken.
+                            More frequent snapshots slow persist operations but make the read operations faster.'
+                        )
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
