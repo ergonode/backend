fd8430593179d5f304f54cdca3f3f5873f66dc40
Mateusz Kolankowski <wiewiurdp@tlen.pl>
2020-12-15 12:50:22 +0100

    bug(attribute) Adding length textarea value validation (#1030)

--- module/attribute/src/Infrastructure/Mapper/Strategy/TextareaAttributeMapperStrategy.php
+++ module/attribute/src/Infrastructure/Mapper/Strategy/TextareaAttributeMapperStrategy.php
@@ -28,6 +28,7 @@ class TextareaAttributeMapperStrategy implements AttributeMapperStrategyInterfac
         foreach ($values as $value) {
             if (null !== $value) {
                 Assert::stringNotEmpty($value);
+                Assert::maxLength($value, 5000);
             }
         }
 
