350940895c5533652a94d66b885cc20975d68ddc
mkolanko <wiewiurdp@tlen.pl>
2021-01-04 12:32:01 +0100

    bug(imports, exports) Adding migrations

---
+++ module/importer/migrations/Version20210104111110.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Ergonode Migration Class:
+ */
+class Version20210104111110 extends AbstractErgonodeMigration
+{
+    /**
+     * @throws \Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            'ALTER TABLE importer.import
+            DROP CONSTRAINT import_source_fk,
+             ADD CONSTRAINT import_source_fk FOREIGN KEY (source_id) 
+             REFERENCES importer.source(id)  ON UPDATE CASCADE ON DELETE CASCADE'
+        );
+    }
+}
