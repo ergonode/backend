281e3949d586804c69f7ce1c1064d7c4f703a519
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2021-01-07 13:06:43 +0100

    feature(exporter) Shopware6 to long SEO attribute value (#1055)

--- module/exporter-shopware-6/src/Application/Form/Shopware6ChannelForm.php
+++ module/exporter-shopware-6/src/Application/Form/Shopware6ChannelForm.php
@@ -213,6 +213,7 @@ class Shopware6ChannelForm extends AbstractType
                 AttributeIdType::class,
                 [
                     'label' => 'Attribute Product Meta Title',
+                    'help' => 'Value in product should contain 255 characters or less.',
                     'choices' => array_flip($textareaAttributeDictionary),
                     'property_path' => 'attributeProductMetaTitle',
                     'required' => false,
@@ -223,6 +224,7 @@ class Shopware6ChannelForm extends AbstractType
                 AttributeIdType::class,
                 [
                     'label' => 'Attribute Product Meta Description',
+                    'help' => 'Value in product should contain 255 characters or less.',
                     'choices' => array_flip($textareaAttributeDictionary),
                     'property_path' => 'attributeProductMetaDescription',
                     'required' => false,
