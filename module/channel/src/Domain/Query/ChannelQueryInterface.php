ece25b65b9187bafe7eef6b105f1df8d45149848
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2021-01-11 09:45:58 +0100

    feature(exporter) Shopware6 lock delete CrossSell (#1061)

--- module/channel/src/Domain/Query/ChannelQueryInterface.php
+++ module/channel/src/Domain/Query/ChannelQueryInterface.php
@@ -15,4 +15,6 @@ use Ergonode\Grid\DataSetInterface;
 interface ChannelQueryInterface
 {
     public function getDataSet(Language $language): DataSetInterface;
+
+    public function findChannelIdsByType(string $type): array;
 }
--- module/channel/src/Infrastructure/Persistence/Query/DbalChannelQuery.php
+++ module/channel/src/Infrastructure/Persistence/Query/DbalChannelQuery.php
@@ -15,10 +15,11 @@ use Ergonode\Channel\Domain\Query\ChannelQueryInterface;
 use Ergonode\Core\Domain\ValueObject\Language;
 use Ergonode\Grid\DataSetInterface;
 use Ergonode\Grid\Factory\DbalDataSetFactory;
+use Ergonode\SharedKernel\Domain\Aggregate\ChannelId;
 
 class DbalChannelQuery implements ChannelQueryInterface
 {
-    private const TABLE_VALUE = 'exporter.channel';
+    private const CHANNEL_TABLE = 'exporter.channel';
 
     private Connection $connection;
 
@@ -35,10 +36,31 @@ class DbalChannelQuery implements ChannelQueryInterface
         return $this->dataSetFactory->create($this->getQuery());
     }
 
+    public function findChannelIdsByType(string $type): array
+    {
+        $qb = $this->connection->createQueryBuilder();
+
+        $data = $qb
+            ->select('id')
+            ->from(self::CHANNEL_TABLE)
+            ->where($qb->expr()->eq('type', ':type'))
+            ->setParameter(':type', $type)
+            ->execute()
+            ->fetchAll(\PDO::FETCH_COLUMN);
+
+        $result = [];
+        foreach ($data as $channelId) {
+            $result[] = new ChannelId($channelId);
+        }
+
+        return $result;
+    }
+
+
     private function getQuery(): QueryBuilder
     {
         return $this->connection->createQueryBuilder()
             ->select('ch.id, ch.name, ch.type')
-            ->from(self::TABLE_VALUE, 'ch');
+            ->from(self::CHANNEL_TABLE, 'ch');
     }
 }
