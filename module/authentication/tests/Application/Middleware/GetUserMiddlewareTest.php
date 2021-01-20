67e99ad83dda4e4f2165eff3a89e3f80d02bacfa
mkolanko <wiewiurdp@tlen.pl>
2021-01-20 09:46:23 +0100

    feature(authentication) Fixing dependencies

--- module/authentication/tests/Application/Middleware/GetUserMiddlewareTest.php
+++ module/authentication/tests/Application/Middleware/GetUserMiddlewareTest.php
@@ -12,7 +12,6 @@ namespace Ergonode\Authentication\Tests\Application\Middleware;
 use Ergonode\Account\Application\Security\Security;
 use Ergonode\Account\Domain\Entity\User;
 use Ergonode\Authentication\Application\Middleware\GetUserMiddleware;
-use Ergonode\EventSourcing\Infrastructure\DomainCommandInterface;
 use Ergonode\SharedKernel\Domain\Aggregate\UserId;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Messenger\Envelope;
@@ -24,14 +23,14 @@ class GetUserMiddlewareTest extends TestCase
     public function testHandle(): void
     {
         $security = $this->createMock(Security::class);
-        $envelope1 = new Envelope($this->createMock(DomainCommandInterface::class));
+        $envelope1 = new Envelope($this->createMock(\stdClass::class));
         $user = $this->createMock(User::class);
         $user->method('getId')->willReturn($this->createMock(UserId::class));
         $security->method('getUser')->willReturn($user);
         $nextMiddleware = $this->createMock(MiddlewareInterface::class);
         $stack = $this->createMock(StackInterface::class);
         $stack->method('next')->willReturn($nextMiddleware);
-        $envelope2 = new Envelope($this->createMock(DomainCommandInterface::class));
+        $envelope2 = new Envelope($this->createMock(\stdClass::class));
         $nextMiddleware->method('handle')->willReturn($envelope2);
 
         $middleware = new GetUserMiddleware($security);
