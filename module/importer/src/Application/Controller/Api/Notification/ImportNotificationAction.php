c9f51ba13ddfdb769d65d6bb995f3dc13afbc59a
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-01-21 11:44:18 +0100

    fix

--- module/importer/src/Application/Controller/Api/Notification/ImportNotificationAction.php
+++ module/importer/src/Application/Controller/Api/Notification/ImportNotificationAction.php
@@ -30,7 +30,7 @@ class ImportNotificationAction
     }
 
     /**
-     * @IsGranted("CHANNEL_READ")
+     * @IsGranted("IMPORT_READ")
      *
      * @SWG\Tag(name="Profile")
      * @SWG\Parameter(
