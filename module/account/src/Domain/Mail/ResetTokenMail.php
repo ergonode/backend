2dd318a96180ae039b0a97050df6b448d8ad90f0
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2021-01-21 12:09:45 +0100

    feature(account) send mail after change pasword (#1083)

---
+++ module/account/src/Domain/Mail/ResetTokenMail.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Account\Domain\Mail;
+
+use Ergonode\Account\Domain\Entity\User;
+use Ergonode\Account\Domain\ValueObject\ResetToken;
+use Ergonode\Mailer\Domain\Mail;
+use Ergonode\Mailer\Domain\Recipient;
+use Ergonode\Mailer\Domain\Sender;
+use Ergonode\Mailer\Domain\Template;
+use Ergonode\SharedKernel\Domain\Collection\EmailCollection;
+
+class ResetTokenMail extends Mail
+{
+    public function __construct(User $user, ResetToken $token, string $url)
+    {
+        $recipient = new Recipient(new EmailCollection([$user->getEmail()]));
+        $template = new Template(
+            '@ErgonodeAccount/message/token.html.twig',
+            $user->getLanguage(),
+            [
+                'url' => $url,
+                'token' => $token->getValue(),
+                'firstName' => $user->getFirstName(),
+            ]
+        );
+
+        parent::__construct($recipient, new Sender(), $template, 'Reset your password at Ergonode PIM');
+    }
+}
