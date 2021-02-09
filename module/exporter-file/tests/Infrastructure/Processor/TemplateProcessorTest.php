5f410733226e3dbefd86c67c9ec199252aede838
Wojciech Fajczyk <w.f.wojtek@gmail.com>
2021-02-09 08:14:05 +0100

    feature(exporter) Ergonode export only template code

---
+++ module/exporter-file/tests/Infrastructure/Processor/TemplateProcessorTest.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\ExporterFile\Tests\Infrastructure\Processor;
+
+use Ergonode\ExporterFile\Infrastructure\Processor\TemplateProcessor;
+use PHPUnit\Framework\TestCase;
+
+class TemplateProcessorTest extends TestCase
+{
+
+}
