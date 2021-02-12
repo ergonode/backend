33abc9fa158bda16da82d406fffedbae837325a7
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-02-12 11:08:35 +0100

    bugfix(attribute) fix scope grid filter values

--- module/attribute/src/Infrastructure/Grid/AttributeGridBuilder.php
+++ module/attribute/src/Infrastructure/Grid/AttributeGridBuilder.php
@@ -108,8 +108,9 @@ class AttributeGridBuilder implements GridBuilderInterface
     {
         $result = [];
         foreach (AttributeScope::AVAILABLE as $item) {
-            $scope[] = new LabelFilterOption($item, $item);
+            $result[] = new LabelFilterOption($item, $item);
         }
+
 
         return $result;
     }
