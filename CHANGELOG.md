aeec3852fbad3bfd05767845875d73ccaf677bb6
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2021-03-03 08:27:34 +0100

    feature(authentication) Created custom ObjectManager for refresh token and decoupled from Doctrine ORM (#1215)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 
+- feature [#1208](https://github.com/ergonode/backend/issues/1208) Created custom ObjectManager for refresh token and decoupled from Doctrine ORM (piotrkreft)
 - ci [#1212](https://github.com/ergonode/backend/issues/1212) Split CI to test code separately (piotrkreft)
 - performance [#1211](https://github.com/ergonode/backend/issues/1211) Cached user authenticated on workers (piotrkreft)
 - feature [#1174](https://github.com/ergonode/backend/issues/1174) Fixed all allowed Composer dependencies and enabled CI jobs to test against boundary conditions (piotrkreft)
---
+++ module/authentication/src/Application/Validator/UniqueRefreshTokenValidator.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Authentication\Application\Validator;
+
+use Ergonode\Authentication\Application\RefreshToken\Doctrine\RefreshTokenRepositoryInterface;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class UniqueRefreshTokenValidator extends ConstraintValidator
+{
+    private RefreshTokenRepositoryInterface $repository;
+
+    public function __construct(RefreshTokenRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$value instanceof RefreshToken) {
+            throw new UnexpectedTypeException($value, RefreshToken::class);
+        }
+        if (!$constraint instanceof UniqueEntity) {
+            throw new UnexpectedTypeException($constraint, UniqueEntity::class);
+        }
+
+        if (!$this->repository->findOneBy(['refreshToken' => $value->getRefreshToken()])) {
+            return;
+        }
+
+        $this->context
+            ->buildViolation('Refresh token already exists')
+            ->addViolation();
+    }
+}
