fa1c132ed08e13503131a1ff66fbc5d528e5f121
Piotr Kreft <kreftpiotrek@gmail.com>
2021-03-05 09:42:43 +0100

    ci: Fixed tests compatibility with constraint message update

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
- * See license.txt for license details.
- */
-
-declare(strict_types=1);
 
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
