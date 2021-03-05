9bf51bbbc578bdbb3b0f31490cbdf269d3ba87d7
Piotr Kreft <kreftpiotrek@gmail.com>
2021-03-05 09:46:37 +0100

    Reverted redundant changes

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See license.txt for license details.
+ */
+
+declare(strict_types=1);
 
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
