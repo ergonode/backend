d2b13ffdf5d89007872a5f85f66f1106f72995d8
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2021-03-19 12:56:02 +0100

    feature(language) Change language list import/export/condition (#1298)

---
+++ module/core/src/Application/Form/Type/LanguageActiveType.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Core\Application\Form\Type;
+
+use Ergonode\Core\Application\Form\DataTransformer\LanguageDataTransformer;
+use Ergonode\Core\Domain\Query\LanguageQueryInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class LanguageActiveType extends AbstractType
+{
+    private TranslatorInterface $translator;
+
+    private LanguageQueryInterface $provider;
+
+    public function __construct(TranslatorInterface $translator, LanguageQueryInterface $provider)
+    {
+        $this->translator = $translator;
+        $this->provider = $provider;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addModelTransformer(new LanguageDataTransformer());
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $languages = $this->provider->getDictionaryActive();
+        $codes = [];
+        foreach ($languages as $code => $name) {
+            $codes[$code] =  $this->translator->trans($name);
+        }
+
+        asort($codes);
+
+        $resolver->setDefaults(
+            [
+                'choices' => array_flip($codes),
+                'invalid_message' => 'Language {{ value }} is not active',
+            ]
+        );
+    }
+
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+}
