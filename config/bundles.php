9bf1affd9fb8f4dcf83c2f76e62c47e88b679a4c
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-03-30 16:09:50 +0200

    feature remove depricated symfony web server

--- config/bundles.php
+++ config/bundles.php
@@ -58,6 +58,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
