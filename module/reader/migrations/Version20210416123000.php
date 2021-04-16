f74c6820f99d841ba618919731b9b13d01e2527e
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-04-16 13:53:33 +0200

    bugfix(importer) remove legacy reader privilege

---
+++ module/reader/migrations/Version20210416123000.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20210416123000 extends AbstractErgonodeMigration
+{
+    /**
+     * @throws \Exception
+     */
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DELETE FROM privileges WHERE area = ?', ['Reader']);
+        $this->addSql('DELETE FROM privileges_group WHERE area = ?', ['Reader']);
+    }
+}
