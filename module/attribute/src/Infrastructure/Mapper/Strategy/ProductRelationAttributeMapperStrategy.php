b2efc9e0f5caaf66001415730eed523780cc8ecd
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-04-21 13:45:42 +0200

    feature(product) add support for editing relation attribute

---
+++ module/attribute/src/Infrastructure/Mapper/Strategy/ProductRelationAttributeMapperStrategy.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Attribute\Infrastructure\Mapper\Strategy;
+
+use Ergonode\Value\Domain\ValueObject\ValueInterface;
+use Ergonode\Attribute\Domain\ValueObject\AttributeType;
+use Webmozart\Assert\Assert;
+use Ergonode\Value\Domain\ValueObject\StringCollectionValue;
+use Ergonode\Product\Domain\Entity\Attribute\ProductRelationAttribute;
+
+class ProductRelationAttributeMapperStrategy implements AttributeMapperStrategyInterface
+{
+    public function supported(AttributeType $type): bool
+    {
+        return $type->getValue() === ProductRelationAttribute::TYPE;
+    }
+
+    public function map(array $values): ValueInterface
+    {
+        Assert::allRegex(array_keys($values), '/^[a-z]{2}_[A-Z]{2}$/');
+
+        foreach ($values as $language => $value) {
+            if (null !== $value && !is_array($value)) {
+                $value = explode(',', (string) $value);
+            }
+
+            if (is_array($value) && !empty($value)) {
+                Assert::allUuid($value);
+                $values[$language] = implode(',', $value);
+            } else {
+                $values[$language] = null;
+            }
+        }
+
+        return new StringCollectionValue($values);
+    }
+}
