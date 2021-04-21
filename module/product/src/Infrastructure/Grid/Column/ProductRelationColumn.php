129e15fcd5ab324945fcba374b8c7fc95ba712e5
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2021-04-21 11:23:12 +0200

    feature(product) Add new grid column for support relation attribute type (#1385)

---
+++ module/product/src/Infrastructure/Grid/Column/ProductRelationColumn.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Product\Infrastructure\Grid\Column;
+
+use Ergonode\Grid\Column\AbstractColumn;
+
+class ProductRelationColumn extends AbstractColumn
+{
+    public const TYPE = 'PRODUCT_RELATION';
+
+    public function __construct(string $field, ?string $label = null)
+    {
+        parent::__construct($field, $label);
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+}
