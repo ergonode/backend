eba5eee98f00006b452f2983f633c0b0bf78735f
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-04-22 15:08:08 +0200

    feature(product) add removing validation strategy for releted products

---
+++ module/product/src/Infrastructure/Persistence/Query/DbalProductRelationAttributeQuery.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Product\Infrastructure\Persistence\Query;
+
+use Doctrine\DBAL\Connection;
+use Ergonode\SharedKernel\Domain\Aggregate\ProductId;
+use Ergonode\Product\Domain\Query\ProductRelationAttributeQueryInterface;
+
+class DbalProductRelationAttributeQuery implements ProductRelationAttributeQueryInterface
+{
+    private Connection $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function findProductRelatedIds(ProductId $id): array
+    {
+        $qb = $this->connection->createQueryBuilder();
+
+        $records = $qb->select('DISTINCT pv.product_id')
+            ->from('value_translation', 'vt')
+            ->where($qb->expr()->like('value', ':id'))
+            ->setParameter(':id', $id->getValue())
+            ->join('vt', 'product_value', 'pv', 'pv.value_id = vt.value_id')
+            ->execute()
+            ->fetchAll(\PDO::FETCH_COLUMN);
+
+        $result = [];
+
+        if ($records) {
+            foreach ($records as $item) {
+                $result[] = new ProductId($item);
+            }
+        }
+
+        return $result;
+    }
+}
