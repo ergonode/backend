634c5d8cae3951411441d094a211c5b8c42c0b6e
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2021-05-18 14:06:07 +0200

    bugfix(workflow) fix incorrect db query for status to product relation (#1441)

---
+++ module/workflow/src/Domain/Query/ProductStatusQueryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Workflow\Domain\Query;
+
+use Ergonode\SharedKernel\Domain\Aggregate\ProductId;
+use Ergonode\SharedKernel\Domain\Aggregate\StatusId;
+
+interface ProductStatusQueryInterface
+{
+    /**
+     * @return ProductId[]
+     */
+    public function findProductIdsByStatusId(StatusId $statusId): array;
+}
