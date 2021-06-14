2350ae88ea6c95940963649dcaa4717743eb3ae0
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2021-06-14 14:31:35 +0200

    bugfix: Allowed disabling default local storages for overriding (#1480)

--- module/multimedia/src/Application/DependencyInjection/Configuration.php
+++ module/multimedia/src/Application/DependencyInjection/Configuration.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright © Ergonode Sp. z o.o. All rights reserved.
  * See LICENSE.txt for license details.
@@ -6,25 +7,24 @@
 
 declare(strict_types=1);
 
-namespace Ergonode\Account\Application\DependencyInjection;
+namespace Ergonode\Multimedia\Application\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-final class Configuration implements ConfigurationInterface
+class Configuration implements ConfigurationInterface
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('ergonode_account');
+        $treeBuilder = new TreeBuilder('ergonode_multimedia');
+
         /** @phpstan-ignore-next-line */
         $treeBuilder
             ->getRootNode()
                 ->children()
-                    // TODO workaround for not being able to set json array from env variables
-                    // see https://github.com/symfony/symfony/issues/27683
-                    ->variableNode('hosts')
-                        ->isRequired()
-                    ->end()
                     ->booleanNode('default_flysystem_config')
                         ->defaultTrue()
                         ->info('Disabled Flysystem configuration defaulting to `local` adapter')
