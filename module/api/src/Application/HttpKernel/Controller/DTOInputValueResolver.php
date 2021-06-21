a40edb57ecdafa0a18ed021c2b7cb125ffb802db
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2021-06-21 11:46:25 +0200

    feature(api) DTO input value resolving (#1498)

---
+++ module/api/src/Application/HttpKernel/Controller/DTOInputValueResolver.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Api\Application\HttpKernel\Controller;
+
+use Ergonode\Api\Application\Exception\ViolationsHttpException;
+use Ergonode\SharedKernel\Application\Api\DTOInputInterface;
+use Ergonode\SharedKernel\Application\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
+use Symfony\Component\Serializer\Exception\RuntimeException;
+use Symfony\Component\Serializer\Exception\UnexpectedValueException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Resolves DTOInputInterface::class input controller argument from request body.
+ */
+class DTOInputValueResolver implements ArgumentValueResolverInterface
+{
+    /**
+     * @var string[]
+     */
+    private array $formatsMap;
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
+
+    /**
+     * @param string[] $formatsMap
+     */
+    public function __construct(
+        array $formatsMap,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ) {
+        $this->formatsMap = $formatsMap;
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+    }
+
+    public function supports(Request $request, ArgumentMetadata $argument): bool
+    {
+        return null !== $argument->getType()
+            && class_exists($argument->getType())
+            && is_subclass_of($argument->getType(), DTOInputInterface::class);
+    }
+
+    /**
+     * @return DTOInputInterface[]|\Traversable
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    {
+        $contentType = $request->headers->get('CONTENT_TYPE');
+
+        if (!$contentType) {
+            throw new BadRequestHttpException('The "Content-Type" header must exist.');
+        }
+        $format = $this->formatsMap[$contentType] ?? null;
+
+        if (!$format) {
+            $message = 'The "Content-Type" unknown. Accepted: '.implode(', ', array_keys($this->formatsMap));
+            throw new UnsupportedMediaTypeHttpException($message);
+        }
+
+        if (!$request->getContent()) {
+            throw new BadRequestHttpException('Missing request body');
+        }
+
+        /** @var string $type */
+        $type = $argument->getType();
+        if ($argument->isVariadic()) {
+            $type .= '[]';
+        }
+
+        try {
+            $dto = $this->serializer->deserialize($request->getContent(), $type, $format);
+        } catch (RuntimeException | UnexpectedValueException $exception) {
+            throw new BadRequestHttpException($exception->getMessage());
+        }
+
+        $violationList = $this->validator->validate($dto);
+        if ($violationList->count()) {
+            throw new ViolationsHttpException($violationList);
+        }
+
+        if (!$argument->isVariadic()) {
+            $dto = [$dto];
+        }
+
+        foreach ($dto as $item) {
+            yield $item;
+        }
+    }
+}
---
+++ module/shared-kernel/src/Application/Api/DTOInputInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\SharedKernel\Application\Api;
+
+/**
+ * Marks a class as DTO Input.
+ */
+interface DTOInputInterface
+{
+}
