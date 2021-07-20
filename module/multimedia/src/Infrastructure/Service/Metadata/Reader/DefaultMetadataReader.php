e7ad73328b90fcff0f3fc889550112ed3128bbc3
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2021-07-20 09:06:02 +0200

    feature(multimedia) extend multimedia information result (#1533)

--- module/multimedia/src/Infrastructure/Service/Metadata/Reader/DefaultMetadataReader.php
+++ module/multimedia/src/Infrastructure/Service/Metadata/Reader/DefaultMetadataReader.php
@@ -19,30 +19,35 @@ class DefaultMetadataReader implements MetadataReaderInterface
      */
     public function read($file): array
     {
-        $imagick = new \Imagick();
-        $imagick->readImageFile($file);
-
         $result = [];
-        $result['width'] = $imagick->getImageWidth();
-        $result['height'] = $imagick->getImageHeight();
-        $resolution = $imagick->getImageResolution();
-        $units = $imagick->getImageUnits();
 
-        $result['alpha'] = false;
-        if ($imagick->getImageAlphaChannel()) {
-            $result['alpha'] = true;
-        }
+        try {
+            $imagick = new \Imagick();
+            $imagick->readImageFile($file);
 
-        if (array_key_exists('x', $resolution) && $resolution['x'] > 0) {
-            if (\Imagick::RESOLUTION_PIXELSPERCENTIMETER === $units) {
-                $result['resolution'] = sprintf('%s pixel/cm', round($resolution['x'], 2));
-            } elseif (\Imagick::RESOLUTION_PIXELSPERINCH === $units) {
-                $result['resolution'] = sprintf('%s pixel/in', round($resolution['x'], 2));
-            } else {
-                $result['resolution'] = sprintf('%s pixel', round($resolution['x'], 2));
+            $result['width'] = $imagick->getImageWidth();
+            $result['height'] = $imagick->getImageHeight();
+            $resolution = $imagick->getImageResolution();
+            $units = $imagick->getImageUnits();
+
+            $result['alpha'] = false;
+            if ($imagick->getImageAlphaChannel()) {
+                $result['alpha'] = true;
             }
-        }
 
-        return $result;
+            if (array_key_exists('x', $resolution) && $resolution['x'] > 0) {
+                if (\Imagick::RESOLUTION_PIXELSPERCENTIMETER === $units) {
+                    $result['resolution'] = sprintf('%s pixel/cm', round($resolution['x'], 2));
+                } elseif (\Imagick::RESOLUTION_PIXELSPERINCH === $units) {
+                    $result['resolution'] = sprintf('%s pixel/in', round($resolution['x'], 2));
+                } else {
+                    $result['resolution'] = sprintf('%s pixel', round($resolution['x'], 2));
+                }
+            }
+
+            return $result;
+        } catch (\Exception $exception) {
+            return [];
+        }
     }
 }
