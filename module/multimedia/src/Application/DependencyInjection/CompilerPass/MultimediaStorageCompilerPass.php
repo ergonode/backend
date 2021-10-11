480c578b98a192861f33be32d3b7efb035100804
Wojciech Fajczyk <w.f.wojtek@gmail.com>
2021-10-11 14:04:47 +0200

    refactor(multimedia) fix compiler pas

--- module/multimedia/src/Application/DependencyInjection/CompilerPass/MultimediaStorageCompilerPass.php
+++ module/multimedia/src/Application/DependencyInjection/CompilerPass/MultimediaStorageCompilerPass.php
@@ -14,18 +14,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MultimediaStorageCompilerPass implements CompilerPassInterface
 {
-    public const TAG = 'multimedia.storage';
-
     public function process(ContainerBuilder $container): void
     {
-        if ($container->has(self::TAG)) {
-            $this->processServices($container);
-        }
-    }
-
-    private function processServices(ContainerBuilder $container): void
-    {
-        $definition = $container->findDefinition(self::TAG);
+        $definition = $container->findDefinition('multimedia.storage');
         $definition->setPublic(true);
     }
 }
