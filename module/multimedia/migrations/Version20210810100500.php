9b146c484a396e7697d6a315182f527d1dc8b4d9
wfajczyk <37833669+wfajczyk@users.noreply.github.com>
2021-10-14 13:58:30 +0200

    refactor(multimedia) Change name migration (#1657)

--- module/multimedia/migrations/Version20210810100500.php
+++ module/multimedia/migrations/Version20210810100500.php
@@ -9,31 +9,40 @@ declare(strict_types=1);
 namespace Ergonode\Migration;
 
 use Doctrine\DBAL\Schema\Schema;
-use Ergonode\Multimedia\Infrastructure\Service\Migration\NameMigrationService;
-use Ergonode\SharedKernel\Domain\Aggregate\MultimediaId;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Ergonode\Multimedia\Domain\Event\MultimediaCreatedEvent;
+use Ergonode\Multimedia\Domain\Event\MultimediaNameChangedEvent;
 
 /**
  * Auto-generated Ergonode Migration Class:
  */
-final class Version20210810100500 extends AbstractErgonodeMigration implements ContainerAwareInterface
+final class Version20210810100500 extends AbstractErgonodeMigration
 {
-    use ContainerAwareTrait;
+    private const MAX_LENGTH = 128;
 
     public function up(Schema $schema): void
     {
-        /** @var NameMigrationService $nameService */
-        $nameService = $this->container
-            ->get(NameMigrationService::class);
+        $nameEventId = $this->connection->executeQuery(
+            'SELECT id FROM event_store_event WHERE event_class = :class',
+            [
+                'class' => MultimediaNameChangedEvent::class,
+            ]
+        )->fetchOne();
 
-        $ids = $this->getIds();
-        foreach ($ids as $id) {
-            try {
-                $nameService->migrateName(new MultimediaId($id));
-            } catch (\Exception $exception) {
-                $this->write($exception->getMessage());
-            }
+        $createEventId = $this->connection->executeQuery(
+            'SELECT id FROM event_store_event WHERE event_class = :class',
+            [
+                'class' => MultimediaCreatedEvent::class,
+            ]
+        )->fetchOne();
+
+        foreach ($this->getIds() as $id => $data) {
+            $oldName = $data['name'];
+
+            $newName = $this->generateName($id, $oldName);
+            $this->updateProjection($id, $newName);
+            $this->updateEvent($id, $nameEventId, $oldName, $newName);
+            $this->updateEvent($id, $createEventId, $oldName, $newName);
+            $this->clearSnapshot($id);
         }
     }
 
@@ -42,7 +51,7 @@ final class Version20210810100500 extends AbstractErgonodeMigration implements C
         return $this->connection
             ->executeQuery(
                 '
-                SELECT m.id FROM multimedia m
+                SELECT m.id, m.name FROM multimedia m
                 WHERE
                       m."name" IN(
                           SELECT m2."name" FROM multimedia m2
@@ -51,6 +60,75 @@ final class Version20210810100500 extends AbstractErgonodeMigration implements C
                       ) 
                 ORDER BY m.created_at ASC'
             )
-            ->fetchFirstColumn();
+            ->fetchAllAssociativeIndexed();
+    }
+
+    private function generateName(string $id, string $name): string
+    {
+        $newName = $name;
+        $i = 0;
+        while ($this->fileExists($id, $newName)) {
+            $newName = $this->generateSuffix($name, $i++);
+        }
+
+        return $newName;
+    }
+
+    private function fileExists(string $id, string $name): bool
+    {
+        return (bool) $this->connection
+            ->executeQuery(
+                'SELECT id FROM multimedia WHERE name = :name AND id <> :id',
+                [
+                    'id' => $id,
+                    'name' => $name,
+                ]
+            )
+            ->fetchOne();
+    }
+
+    private function generateSuffix(string $name, int $iterationIndex): string
+    {
+        $suffix = '('.$iterationIndex.')';
+        if (mb_strlen($name) > (self::MAX_LENGTH - mb_strlen($suffix))) {
+            return mb_substr($name, 0, self::MAX_LENGTH - mb_strlen($suffix)).$suffix;
+        }
+
+        return $name.$suffix;
+    }
+
+    private function updateProjection(string $id, string $name): void
+    {
+        $this->connection->executeQuery(
+            'UPDATE multimedia SET name = :name WHERE id = :id',
+            [
+                'id' => $id,
+                'name' => $name,
+            ]
+        );
+    }
+
+    private function updateEvent(string $id, string $eventId, string $oldName, string $newName): void
+    {
+        $this->connection->executeQuery(
+            'UPDATE event_store SET payload = jsonb_set(payload, \'{name}\', :newName) 
+                     WHERE aggregate_id = :id AND event_id = :event ',
+            [
+                'oldName' => $oldName,
+                'newName' => json_encode($newName),
+                'id' => $id,
+                'event' => $eventId,
+            ]
+        );
+    }
+
+    private function clearSnapshot(string $id): void
+    {
+        $this->connection->executeQuery(
+            'DELETE FROM event_store_snapshot WHERE aggregate_id = :id',
+            [
+                'id' => $id,
+            ],
+        );
     }
 }
