d31f1ed630c9c645c8f4f3a25cffce887e619db0
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2021-10-25 15:00:52 +0200

    test(import) add multimedia import tests (#1679)

---
+++ module/core/src/Test/TestDownloaderDecorator.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Core\Test;
+
+use Ergonode\Core\Infrastructure\Service\DownloaderInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class TestDownloaderDecorator implements DownloaderInterface
+{
+    private DownloaderInterface $downloader;
+
+    private KernelInterface $kernel;
+
+    public function __construct(DownloaderInterface $downloader, KernelInterface $kernel)
+    {
+        $this->downloader = $downloader;
+        $this->kernel = $kernel;
+    }
+
+
+    public function download(string $url, array $headers = []): ?string
+    {
+        if (false === strpos($url, 'file://')) {
+            return $this->downloader->download($url, $headers);
+        }
+
+        $path = $this->kernel->getProjectDir().'/'.substr($url, 6);
+
+        return file_get_contents($path);
+    }
+}
