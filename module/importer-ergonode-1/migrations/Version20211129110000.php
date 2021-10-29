8bd84b25a41f7af060c692f4f9cd674c8af7dd01
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2021-10-29 11:32:46 +0200

    bugfix(import) add missing ergonode-zip source migration (#1683)

---
+++ module/importer-ergonode-1/migrations/Version20211129110000.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Ergonode Migration Class:
+ */
+final class Version20211129110000 extends AbstractErgonodeMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('UPDATE importer.source 
+            SET configuration =  configuration::jsonb || \'{"headers" : []}\'::jsonb WHERE type = \'ergonode-zip\'');
+    }
+}
