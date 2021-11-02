fe4df0475fae197a9496c25a28b1ec45912259d4
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-11-02 14:44:26 +0100

    feature(attribute) add new option-attribute relation table migration

---
+++ module/attribute/tests/Domain/Event/Option/OptionMovedEventTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types = 1);
+
+namespace Ergonode\Attribute\Tests\Domain\Event\Option;
+
+use PHPUnit\Framework\TestCase;
+use Ergonode\SharedKernel\Domain\AggregateId;
+use Ergonode\SharedKernel\Domain\Aggregate\AttributeId;
+use Ergonode\Attribute\Domain\Event\Option\OptionMovedEvent;
+
+class OptionMovedEventTest extends TestCase
+{
+    public function testEventCreation(): void
+    {
+        /** @var AggregateId $id */
+        $optionId = $this->createMock(AggregateId::class);
+        /** @var AttributeId $attributeId */
+        $attributeId = $this->createMock(AttributeId::class);
+
+        $event = new OptionMovedEvent($attributeId, $optionId);
+        $this->assertEquals($attributeId, $event->getAggregateId());
+        $this->assertEquals($optionId, $event->getOptionId());
+        $this->assertTrue($event->isAfter());
+    }
+}
