651da05668147d564c33301d02e9c4e34754ec93
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2021-12-02 09:21:14 +0100

    feature(workflow) add LanguageAttributeExists condition (#1758)

---
+++ module/condition/src/Infrastructure/Condition/Calculator/AttributeTranslationExistsConditionCalculatorStrategy.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Condition\Infrastructure\Condition\Calculator;
+
+use Ergonode\Attribute\Domain\Repository\AttributeRepositoryInterface;
+use Ergonode\Condition\Domain\ConditionInterface;
+use Ergonode\Condition\Infrastructure\Condition\ConditionCalculatorStrategyInterface;
+use Ergonode\Product\Domain\Entity\AbstractProduct;
+use Webmozart\Assert\Assert;
+use Ergonode\Condition\Domain\Condition\AttributeTranslationExistsCondition;
+use Ergonode\Product\Infrastructure\Calculator\TranslationInheritanceCalculator;
+
+class AttributeTranslationExistsConditionCalculatorStrategy implements ConditionCalculatorStrategyInterface
+{
+    private AttributeRepositoryInterface $repository;
+
+    private TranslationInheritanceCalculator $calculator;
+
+    public function __construct(AttributeRepositoryInterface $repository, TranslationInheritanceCalculator $calculator)
+    {
+        $this->repository = $repository;
+        $this->calculator = $calculator;
+    }
+
+    public function supports(string $type): bool
+    {
+        return AttributeTranslationExistsCondition::TYPE === $type;
+    }
+
+    public function calculate(AbstractProduct $object, ConditionInterface $configuration): bool
+    {
+        if (!$configuration instanceof AttributeTranslationExistsCondition) {
+            throw new \LogicException(
+                sprintf(
+                    'Expected an instance of %s. %s received.',
+                    AttributeTranslationExistsCondition::class,
+                    get_debug_type($configuration)
+                )
+            );
+        }
+        $attributeId = $configuration->getAttribute();
+        $language = $configuration->getLanguage();
+        $attribute = $this->repository->load($attributeId);
+
+        Assert::notNull($attribute);
+
+        $code = $attribute->getCode();
+
+        if ($object->hasAttribute($code)) {
+            $value = $this->calculator->calculate($attribute->getScope(), $object->getAttribute($code), $language);
+            if (null !== $value && [] !== $value) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ module/condition/src/Infrastructure/Condition/Validator/AttributeTranslationExistsConditionValidatorStrategy.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Condition\Infrastructure\Condition\Validator;
+
+use Ergonode\Attribute\Application\Validator\AttributeExists;
+use Ergonode\Condition\Infrastructure\Condition\ConditionValidatorStrategyInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Ergonode\Core\Application\Validator\LanguageCodeActive;
+use Ergonode\Condition\Domain\Condition\AttributeTranslationExistsCondition;
+
+class AttributeTranslationExistsConditionValidatorStrategy implements ConditionValidatorStrategyInterface
+{
+    public function supports(string $type): bool
+    {
+        return $type === AttributeTranslationExistsCondition::TYPE;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function build(array $data): Constraint
+    {
+        return new Collection(
+            [
+                'attribute' => [
+                    new NotBlank(),
+                    new AttributeExists(),
+                ],
+                'language' => [
+                    new NotBlank(),
+                    new LanguageCodeActive(),
+                ],
+            ]
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getValidatedClass(): string
+    {
+        return AttributeTranslationExistsCondition::class;
+    }
+}
