4494f7cc91e89fd1ce9339d447b7a854e2826ea2
Piotr Kreft <49146985+piotrkreft@users.noreply.github.com>
2021-12-10 15:45:03 +0100

    bugfix(completeness,segment) Fixed not recalculating segment for product on completeness update (#1781)

---
+++ module/segment/src/Infrastructure/Handler/Event/Application/ProductCompletenessCalculatedEventHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Segment\Infrastructure\Handler\Event\Application;
+
+use Ergonode\Completeness\Application\Event\ProductCompletenessCalculatedEvent;
+use Ergonode\Segment\Infrastructure\Service\SegmentProductService;
+
+class ProductCompletenessCalculatedEventHandler
+{
+    private SegmentProductService $segmentService;
+
+    public function __construct(SegmentProductService $segmentService)
+    {
+        $this->segmentService = $segmentService;
+    }
+
+    public function __invoke(ProductCompletenessCalculatedEvent $event): void
+    {
+        $this->segmentService->recalculateProduct($event->getProductId());
+    }
+}
