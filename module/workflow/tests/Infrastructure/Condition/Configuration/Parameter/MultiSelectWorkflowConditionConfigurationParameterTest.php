3e88e4ca55ebb8a87d9e6e99333555105498f9a8
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-12-15 12:43:04 +0100

    feature(workflow) add WorkflwoConfigurationInterface support

--- module/workflow/tests/Infrastructure/Condition/Configuration/Parameter/MultiSelectWorkflowConditionConfigurationParameterTest.php
+++ module/workflow/tests/Infrastructure/Condition/Configuration/Parameter/MultiSelectWorkflowConditionConfigurationParameterTest.php
@@ -9,7 +9,8 @@ declare(strict_types=1);
 namespace Ergonode\Workflow\Tests\Infrastructure\Condition\Configuration\Parameter;
 
 use PHPUnit\Framework\TestCase;
-use Ergonode\Workflow\Infrastructure\Condition\Configuration\Parameter\MultiSelectWorkflowConditionConfigurationParameter;
+use Ergonode\Workflow\Infrastructure\Condition\Configuration\Parameter\
+MultiSelectWorkflowConditionConfigurationParameter;
 
 class MultiSelectWorkflowConditionConfigurationParameterTest extends TestCase
 {
