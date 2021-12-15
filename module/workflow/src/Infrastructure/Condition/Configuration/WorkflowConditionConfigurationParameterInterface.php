c63ee36a4692685a01a7c4216c4fb6871ad6c76b
Rafał Przędzik <rafalprzedzik@poczta.onet.pl>
2021-12-15 11:05:14 +0100

    feature(workflow) add WorkflwoConfigurationInterface support

---
+++ module/workflow/src/Infrastructure/Condition/Configuration/WorkflowConditionConfigurationParameterInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Workflow\Infrastructure\Condition\Configuration;
+
+class WorkflowConditionConfigurationParameterInterface
+{
+
+}
