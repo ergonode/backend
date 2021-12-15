62138b0d5ed148fa0dfde97f92bdbc5cd9e5ac32
Mateusz Kolankowski <wiewiurdp@tlen.pl>
2021-12-15 10:06:57 +0100

    feature(workflow) - Adding index column in status projection table (#1790)

---
+++ module/workflow/migrations/Version20211214160000.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Ergonode Migration Class:
+ */
+final class Version20211214160000 extends AbstractErgonodeMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE status ADD COLUMN index SERIAL');
+    }
+}
