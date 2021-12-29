660ff9bc92c9dee5dabd22864283713d1fde55c3
Piotr Kreft <kreftpiotrek@gmail.com>
2021-12-29 08:53:01 +0100

    docs: Updated copyrights

--- module/core/tests/Domain/Event/UnitNameChangedEventTest.php
+++ module/core/tests/Domain/Event/UnitNameChangedEventTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright © Bold Brand Commerce Sp. z o.o. All rights reserved.
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
  * See LICENSE.txt for license details.
  */
 
