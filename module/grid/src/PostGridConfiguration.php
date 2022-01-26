0337183238037ffb08895ada0f29ae9f23562830
Mateusz Kolankowski <wiewiurdp@tlen.pl>
2022-01-26 14:46:15 +0100

    bugfix(grid) - Fixing POST grid filtering by "0" (#1869)

--- module/grid/src/PostGridConfiguration.php
+++ module/grid/src/PostGridConfiguration.php
@@ -56,7 +56,7 @@ class PostGridConfiguration implements GridConfigurationInterface
 
             $column = $filter['column'];
             $operator = $filter['operator'];
-            $value = $filter['value'] ? (string) $filter['value']: null;
+            $value = (isset($filter['value']) && "" !== $filter['value']) ? (string) $filter['value'] : null;
             $language = array_key_exists('language', $filter) ? new Language($filter['language']) : null;
             $key = $filter['column'];
             if ($language) {
